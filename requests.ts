d2be33ab2afbf8b8b8719beb27583e3d5975fbbb
Roj <ez@roj.im>
2023-04-08 18:20:55 +0300

    Implement new TL (de)serialization APIs (#2)

--- requests.ts
+++ requests.ts
@@ -1,9 +1,5 @@
 import { Transport } from "./transport/transport.ts";
-import {
-  deserializeString,
-  packUnencryptedMessage,
-  serializeString,
-} from "./utilities/1_tl.ts";
+import { packUnencryptedMessage } from "./utilities/1_tl.ts";
 import {
   bigIntFromBuffer,
   getRandomBigInt,
@@ -11,7 +7,6 @@ import {
 } from "./utilities/0_bigint.ts";
 import { sha1, sha256 } from "./utilities/0_hash.ts";
 import { bufferFromBigInt, concat, xor } from "./utilities/0_buffer.ts";
-import { ExtendedDataView } from "./utilities/0_extended_data_view.ts";
 import {
   assertEquals,
   factorize,
@@ -19,6 +14,8 @@ import {
   ige256Encrypt,
   randomBigIntBits,
 } from "./deps.ts";
+import { TLReader } from "./utilities/1_tl_reader.ts";
+import { TLWriter } from "./utilities/1_tl_writer.ts";
 
 const req_pq_multi = 0xbe7e8ef1;
 const p_q_inner_data = 0x83c95aec;
@@ -28,50 +25,50 @@ const set_client_DH_params = 0xf5045f1f;
 
 const PUBLIC_KEYS = [
   {
-    f: 0xd09d1d85de64fd85n,
+    f: -0x2f62e27a219b027bn,
     n: 0xe8bb3305c0b52c6cf2afdf7637313489e63e05268e5badb601af417786472e5f93b85438968e20e6729a301c0afc121bf7151f834436f7fda680847a66bf64accec78ee21c0b316f0edafe2f41908da7bd1f4a5107638eeb67040ace472a14f90d9f7c2b7def99688ba3073adb5750bb02964902a359fe745d8170e36876d4fd8a5d41b2a76cbff9a13267eb9580b2d06d10357448d20d9da2191cb5d8c93982961cdfdeda629e37f1fb09a0722027696032fe61ed663db7a37f6f263d370f69db53a0dc0a1748bdaaff6209d5645485e6e001d1953255757e4b8e42813347b11da6ab500fd0ace7e6dfa3736199ccaf9397ed0745a427dcfa6cd67bcb1acff3n,
     e: 0x10001n,
   },
   {
-    f: 0xb25898df208d2603n,
+    f: -0x4da76720df72d9fdn,
     n: 0xc8c11d635691fac091dd9489aedced2932aa8a0bcefef05fa800892d9b52ed03200865c9e97211cb2ee6c7ae96d3fb0e15aeffd66019b44a08a240cfdd2868a85e1f54d6fa5deaa041f6941ddf302690d61dc476385c2fa655142353cb4e4b59f6e5b6584db76fe8b1370263246c010c93d011014113ebdf987d093f9d37c2be48352d69a1683f8f6e6c2167983c761e3ab169fde5daaa12123fa1beab621e4da5935e9c198f82f35eae583a99386d8110ea6bd1abb0f568759f62694419ea5f69847c43462abef858b4cb5edc84e7b9226cd7bd7e183aa974a712c079dde85b9dc063b8a5c08e8f859c0ee5dcd824c7807f20153361a7f63cfd2a433a1be7f5n,
     e: 0x10001n,
   },
   {
-    f: 0xb0419d69dc08a72cn,
+    f: -0x4fbe629623f758d4n,
     n: 0xCE04512E9D3DBB6EA8DFEE9B560C3C48E406E7852DD9640A019FCDA0FB65F69A4264B60CCAC82162A954A27A1CE764CD81389DD07D314A2E7B078F403E6D9DE57346772F603D9383A0835674070E35CD7FB965747F8B27EE69F4B191CA003B40D9D22AC574A35A2C23E5A79B185069106752979720B1DC6E8C8E5F3FE667CA45B5CB7033612AA08F92A67BA04345DDC8F5603688A9559F1BDA2F053BD2C498510DE0AB528FED53BCAE548F40D36EACC292D1D3AEE5AA06C87185397BC73CAF642F731B9C9323F8D658B9C0484F42EF77438113265CC6AE883063A8B7FC89B183B48192C6A423E96AAA80169965B79B0C497623577CD5086B386D4CA5C44B3DFDn,
-    e: 0x010001n,
+    e: 0x10001n,
   },
 ];
 
 export async function reqPqMulti(transport: Transport) {
-  const nonce = randomBigIntBits(16 * 8);
+  const nonce = getRandomBigInt(16, false, true);
 
   await transport.send(
     packUnencryptedMessage(
-      concat(
-        bufferFromBigInt(req_pq_multi, 4),
-        bufferFromBigInt(nonce, 16),
-      ),
+      new TLWriter()
+        .writeInt32(req_pq_multi)
+        .writeInt128(nonce)
+        .buffer,
     ),
   );
 
   const buffer = await transport.receive();
-  const dataView = new ExtendedDataView(buffer.buffer);
+  const reader = new TLReader(buffer);
 
-  const _authKeyId = dataView.getBigUint64(0, true);
-  const _messageId = dataView.getBigUint64(8, true);
-  const _messageLength = dataView.getUint32(16, true);
-  const _constructorId = dataView.getUint32(20, true);
-  const nonce_ = dataView.getBigUint128(24, true);
+  const _authKeyId = reader.readInt64();
+  const _messageId = reader.readInt64();
+  const _messageLength = reader.readInt32();
+  const _constructorId = reader.readInt32();
+  const nonce_ = reader.readInt128();
   assertEquals(nonce, nonce_);
-  const serverNonce = dataView.getBigUint128(40, true);
-  const pq_ = deserializeString(buffer.slice(56, 56 + 12));
-  const _vectorConstructor = dataView.getUint32(68, true);
-  const _count = dataView.getUint32(72, true);
-  const publicKeyFingerprint = dataView.getBigUint64(76, true);
+  const serverNonce = reader.readInt128();
+  const pq_ = reader.readBytes();
+  const _vectorConstructor = reader.readInt32();
+  const _count = reader.readInt32();
+  const publicKeyFingerprint = reader.readInt64();
 
-  const pq = bigIntFromBuffer(pq_);
+  const pq = bigIntFromBuffer(pq_, false, true);
 
   return { nonce, serverNonce, publicKeyFingerprint, pq, pqBytes: pq_ };
 }
@@ -92,15 +89,15 @@ export async function getDHParams(
   const [p, q] = factorize(pq);
   const newNonce = randomBigIntBits(32 * 8);
 
-  let data = concat(
-    bufferFromBigInt(p_q_inner_data, 4),
-    serializeString(pqBytes),
-    serializeString(p.valueOf(), 4),
-    serializeString(q.valueOf(), 4),
-    bufferFromBigInt(nonce, 16),
-    bufferFromBigInt(serverNonce, 16),
-    bufferFromBigInt(newNonce, 32),
-  );
+  let data = new TLWriter()
+    .writeInt32(p_q_inner_data)
+    .writeBytes(pqBytes)
+    .writeBytes(bufferFromBigInt(p.valueOf(), 4, false))
+    .writeBytes(bufferFromBigInt(q.valueOf(), 4, false))
+    .writeInt128(nonce)
+    .writeInt128(serverNonce)
+    .writeInt256(newNonce)
+    .buffer;
 
   /// Step 1
   /// data_with_padding := data + random_padding_bytes; — where random_padding_bytes are chosen so that the resulting length of data_with_padding is precisely 192 bytes, and data is the TL-serialized data to be encrypted as before. One has to check that data is not longer than 144 bytes.
@@ -164,7 +161,7 @@ export async function getDHParams(
     assertEquals(keyAesEncrypted.length, 256);
     /// End step 7
 
-    iKeyAesEncrypted = bigIntFromBuffer(keyAesEncrypted);
+    iKeyAesEncrypted = bigIntFromBuffer(keyAesEncrypted, false, false);
   } while (iKeyAesEncrypted >= serverKey);
   /// The value of key_aes_encrypted is compared with the RSA-modulus of server_pubkey as a big-endian 2048-bit (256-byte) unsigned integer. If key_aes_encrypted turns out to be greater than or equal to the RSA modulus, the previous steps starting from the generation of new random temp_key are repeated.
   // if (iKeyAesEncrypted >= serverKey) {
@@ -176,84 +173,85 @@ export async function getDHParams(
   /// encrypted_data := RSA(key_aes_encrypted, server_pubkey); — 256-byte big-endian integer is elevated to the requisite power from the RSA public key modulo the RSA modulus, and the result is stored as a big-endian integer consisting of exactly 256 bytes (with leading zero bytes if required).
   const encrypedData = modExp(iKeyAesEncrypted, exponent, serverKey);
 
-  assertEquals(bufferFromBigInt(encrypedData, 256, false).length, 256);
+  const encryptedDataBuf = bufferFromBigInt(encrypedData, 256, false, true);
+
+  assertEquals(encryptedDataBuf.length, 256);
   /// End step 8
 
-  const encryptedDataBuf = bufferFromBigInt(encrypedData, 256, false);
 
   await transport.send(packUnencryptedMessage(
-    concat(
-      bufferFromBigInt(req_DH_params, 4),
-      bufferFromBigInt(nonce, 16),
-      bufferFromBigInt(serverNonce, 16),
-      serializeString(p.valueOf(), 4),
-      serializeString(q.valueOf(), 4),
-      bufferFromBigInt(publicKeyFingerprint, 8),
-      serializeString(encryptedDataBuf),
-    ),
+    new TLWriter()
+      .writeInt32(req_DH_params)
+      .writeInt128(nonce)
+      .writeInt128(serverNonce)
+      .writeBytes(bufferFromBigInt(p.valueOf(), 4, false))
+      .writeBytes(bufferFromBigInt(q.valueOf(), 4, false))
+      .writeInt64(publicKeyFingerprint)
+      .writeBytes(encryptedDataBuf)
+      .buffer,
   ));
 
   const buffer = await transport.receive();
-  let dataView = new ExtendedDataView(buffer.buffer);
+  let reader = new TLReader(buffer);
 
-  const _authKeyId = dataView.getBigUint64(0, true);
-  const _messageId = dataView.getBigUint64(8, true);
-  const _messageLength = dataView.getUint32(16, true);
-  const _constructorId = dataView.getUint32(20, true);
-  const _nonce_ = dataView.getBigUint128(24, true);
-  const _serverNonce_ = dataView.getBigUint128(40, true);
-  const encryptedAnswer = deserializeString(buffer.slice(56, 56 + 596));
+  const _authKeyId = reader.readInt64();
+  const _messageId = reader.readInt64();
+  const _messageLength = reader.readInt32();
+  const _constructorId = reader.readInt32();
+  const _nonce_ = reader.readInt128();
+  const _serverNonce_ = reader.readInt128();
+  const encryptedAnswer = reader.readBytes();
 
   const tmpAesKey = concat(
     await sha1(
       concat(
-        bufferFromBigInt(newNonce, 32),
-        bufferFromBigInt(serverNonce, 16),
+        bufferFromBigInt(newNonce, 32, true, true),
+        bufferFromBigInt(serverNonce, 16, true, true),
       ),
     ),
     (await sha1(
       concat(
-        bufferFromBigInt(serverNonce, 16),
-        bufferFromBigInt(newNonce, 32),
+        bufferFromBigInt(serverNonce, 16, true, true),
+        bufferFromBigInt(newNonce, 32, true, true),
       ),
     )).slice(0, 12),
   );
 
   const tmpAesIv = concat(
     (await sha1(concat(
-      bufferFromBigInt(serverNonce, 16),
-      bufferFromBigInt(newNonce, 32),
+      bufferFromBigInt(serverNonce, 16, true, true),
+      bufferFromBigInt(newNonce, 32, true, true),
     ))).slice(12, 12 + 8),
     await sha1(concat(
-      bufferFromBigInt(newNonce, 32),
-      bufferFromBigInt(newNonce, 32),
+      bufferFromBigInt(newNonce, 32, true, true),
+      bufferFromBigInt(newNonce, 32, true, true),
     )),
-    bufferFromBigInt(newNonce, 32).slice(0, 4),
+    bufferFromBigInt(newNonce, 32, true, true).slice(0, 4),
   );
 
-  const answer = ige256Decrypt(encryptedAnswer, tmpAesKey, tmpAesIv).slice(20);
+  const _answer = ige256Decrypt(encryptedAnswer, tmpAesKey, tmpAesIv).slice(20);
 
-  dataView = new ExtendedDataView(answer.buffer);
+  reader = new TLReader(buffer);
 
-  const _constructorId_ = dataView.getUint32(0, true);
-  const __nonce = dataView.getBigUint128(4, true);
-  const __serverNonce = dataView.getBigUint128(20, true);
-  const g = dataView.getUint32(36, true);
-  const dhPrime = deserializeString(answer.slice(40, 40 + 260));
-  const gA = deserializeString(answer.slice(300, 300 + 260));
-  const _serverTime = dataView.getUint32(560);
+  const _constructorId_ = reader.readInt32();
+  const __nonce = reader.readInt128();
+  const __serverNonce = reader.readInt128();
+  const g = reader.readInt32();
+  const dhPrime = reader.readBytes();
+  const gA = reader.readBytes();
+  const _serverTime = reader.readInt32();
 
-  const b = getRandomBigInt(256);
+  const b = getRandomBigInt(256, false, false);
 
-  const gB = modExp(BigInt(g), b, bigIntFromBuffer(dhPrime));
+  const gB = modExp(BigInt(g), b, bigIntFromBuffer(dhPrime, false, true));
 
-  data = concat(
-    bufferFromBigInt(client_DH_inner_data, 4),
-    bufferFromBigInt(nonce, 16),
-    bufferFromBigInt(serverNonce, 16),
-    bufferFromBigInt(0, 8),
-    serializeString(bufferFromBigInt(gB, 256, false)),
-  );
+  data = new TLWriter()
+    .writeInt32(client_DH_inner_data)
+    .writeInt128(nonce)
+    .writeInt128(serverNonce)
+    .writeInt64(0n)
+    .writeBytes(bufferFromBigInt(gB, 256, false, true))
+    .buffer;
 
   let dataWithHash = concat(await sha1(data), data);
 
@@ -268,15 +266,19 @@ export async function getDHParams(
   const encryptedData = ige256Encrypt(dataWithHash, tmpAesKey, tmpAesIv);
 
   await transport.send(packUnencryptedMessage(
-    concat(
-      bufferFromBigInt(set_client_DH_params, 4),
-      bufferFromBigInt(nonce, 16),
-      bufferFromBigInt(serverNonce, 16),
-      serializeString(encryptedData),
-    ),
+    new TLWriter()
+      .writeInt32(set_client_DH_params)
+      .writeInt128(nonce)
+      .writeInt128(serverNonce)
+      .writeBytes(encryptedData)
+      .buffer,
   ));
 
-  const authKey = modExp(bigIntFromBuffer(gA), b, bigIntFromBuffer(dhPrime));
+  const authKey = modExp(
+    bigIntFromBuffer(gA, false, true),
+    b,
+    bigIntFromBuffer(dhPrime, false, true),
+  );
 
   return authKey;
 }
--- utilities/1_tl.ts
+++ utilities/1_tl.ts
@@ -1,65 +1,6 @@
-import { bufferFromBigInt, concat } from "./0_buffer.ts";
-import { ExtendedDataView } from "./0_extended_data_view.ts";
-
-export function deserializeString(buffer: Uint8Array) {
-  let L = buffer[0];
-  if (L <= 253) {
-    return buffer.slice(1, L + 1);
-  }
-
-  const dataView = new ExtendedDataView(buffer.buffer);
-  L = dataView.getInt24(0);
-
-  return buffer.slice(4).slice(0, L);
-}
-
-export function serializeString(bytes: bigint, byteLength: number): Uint8Array;
-export function serializeString(bytes: Uint8Array): Uint8Array;
-export function serializeString(
-  bytes: Uint8Array | bigint,
-  byteLength?: number,
-) {
-  if (typeof bytes === "bigint") {
-    if (!byteLength) {
-      throw new Error("byteLength is unspecified");
-    } else {
-      bytes = bufferFromBigInt(bytes, byteLength, false);
-    }
-  }
-
-  let padding = 0;
-
-  if (bytes.length <= 253) {
-    padding = (bytes.length + 1) % 4;
-  } else {
-    padding = bytes.length % 4;
-  }
-
-  if (padding > 0) {
-    padding = 4 - padding;
-  }
-
-  if (bytes.length <= 253) {
-    return concat(
-      new Uint8Array([bytes.length]),
-      bytes,
-      new Uint8Array(padding),
-    );
-  } else {
-    const length = new Uint8Array(3);
-
-    length[0] = (bytes.length & 0xff0000) >>> 16;
-    length[1] = (bytes.length & 0x00ff00) >>> 8;
-    length[2] = bytes.length & 0x0000ff;
-
-    return concat(
-      new Uint8Array([254]),
-      length.reverse(),
-      bytes,
-      new Uint8Array(padding),
-    );
-  }
-}
+import { ige256Encrypt } from "../../tgcrypto_wasm/dist/mod.ts";
+import { getRandomBigInt, mod } from "./0_bigint.ts";
+import { bufferFromBigInt, concat, sha1, sha256 } from "./0_buffer.ts";
 
 let lastMsgId = 0n;
 export function getMessageId() {
@@ -82,4 +23,56 @@ export function packUnencryptedMessage(data: Uint8Array) {
     data,
   );
   return message;
+}
+
+export async function packEncryptedMessage(data: Uint8Array, authKey: bigint) {
+  const salt = getRandomBigInt(8, true, false);
+  const sessionId = getRandomBigInt(8, true, false);
+  const messageId = getMessageId();
+  const seqNo = bufferFromBigInt(1, 4);
+  const messageDataLength = bufferFromBigInt(data.length, 4);
+
+  let message = concat(
+    bufferFromBigInt(salt, 8),
+    bufferFromBigInt(sessionId, 8),
+    bufferFromBigInt(messageId, 8),
+    seqNo,
+    messageDataLength,
+    data,
+  );
+
+  const padding = new Uint8Array(mod(-(message.length + 12), 16) + 12);
+
+  crypto.getRandomValues(padding);
+
+  message = concat(message, padding);
+
+  const x = 0;
+
+  const authKeyBuffer = bufferFromBigInt(authKey, 256, false);
+  const authKeyId = (await sha1(authKeyBuffer)).slice(-8);
+
+  // const middleSha256 = (await sha256(message)).slice(8).slice(0, -8);
+  const authKeySubstr = authKeyBuffer.slice(88 + x, 88 + 32 + x);
+  const randomPadding = new Uint8Array(mod(-(message.length + 12), 16) + 12);
+  const msgKeyLarge = concat(authKeySubstr, data, randomPadding);
+  const msgKey = msgKeyLarge.slice(8, 8 + 16);
+
+  const sha256A = await sha256(concat(msgKey, authKeyBuffer.slice(x, x + 36)));
+  const sha256B = await sha256(
+    concat(authKeyBuffer.slice(40 + x, 40 + x + 36), msgKey),
+  );
+  const aesKey = concat(
+    sha256A.slice(0, 8),
+    sha256B.slice(8, 8 + 16),
+    sha256A.slice(24, 24 + 8),
+  );
+  const aesIv = concat(
+    sha256B.slice(0, 8),
+    sha256A.slice(8, 8 + 16),
+    sha256B.slice(24, 24 + 8),
+  );
+
+  const encryptedMessage = ige256Encrypt(message, aesKey, aesIv);
+  return concat(authKeyId, msgKey, encryptedMessage);
 }
