5a0036c7b3f2872e63a2cf613185ff0e4cf3ac4f
Roj <ez@roj.im>
2023-04-09 22:24:26 +0300

    Implement vector serialization

--- tl/1_tl_object.ts
+++ tl/1_tl_object.ts
@@ -50,6 +50,75 @@ export const params = Symbol("params");
 
 export const paramDesc = Symbol("paramDesc");
 
+function serializeSingleParam(
+  writer: TLRawWriter,
+  value: Param,
+  type:
+    | typeof Uint8Array
+    | "string"
+    | "number"
+    | "bigint"
+    | "boolean"
+    | "true",
+  note: string,
+) {
+  if (type == Uint8Array) {
+    if ((value instanceof Uint8Array)) {
+      writer.writeBytes(value);
+    } else {
+      throw new Error("Expected Uint8Array");
+    }
+  }
+
+  switch (type) {
+    case "bigint":
+      if (typeof value === "bigint") {
+        if (note == "int128") {
+          writer.writeInt128(value);
+        } else if (note === "int256") {
+          writer.writeInt256(value);
+        } else {
+          writer.writeInt64(value);
+        }
+      } else {
+        throw new Error("Expected bigint");
+      }
+      break;
+    case "boolean":
+      if (typeof value === "boolean") {
+        if (value) {
+          writer.writeInt32(0x997275b5);
+        } else {
+          writer.writeInt32(0xbc799737);
+        }
+      } else {
+        throw new Error("Expected boolean");
+      }
+      break;
+    case "number":
+      if (typeof value === "number") {
+        writer.writeInt32(value);
+      } else {
+        throw new Error("Expected number");
+      }
+      break;
+    case "string":
+      if (typeof value === "string") {
+        writer.writeString(value);
+      } else if (value instanceof Uint8Array) {
+        writer.writeBytes(value);
+      } else {
+        throw new Error("Expected string or Uint8Array");
+      }
+      break;
+    case "true":
+      if (value === true) {
+        writer.writeInt32(0x997275b5);
+      } else {
+        throw new Error("Expected true");
+      }
+  }
+}
 export abstract class TLObject {
   protected abstract get [id](): number;
   protected abstract get [params](): Params;
@@ -64,68 +133,27 @@ export abstract class TLObject {
 
     for (const [value, type, note] of this[params]) {
       if (type instanceof Array) {
-        throw new Error("Unimplemented");
-      }
-      if (type instanceof TLObject) {
-        throw new Error("Unimplemented");
-      }
-
-      if (type == Uint8Array) {
-        if ((value instanceof Uint8Array)) {
-          writer.writeBytes(value);
-        } else {
-          throw new Error("Expected Uint8Array");
+        const itemsType = type[0];
+        if (isTLObjectConstructor(itemsType)) {
+          throw new Error("Unimplemented");
         }
+
+        if (!Array.isArray(value)) {
+          throw new Error("Expected array");
+        }
+        writer.writeInt32(0x1cb5c415); // vector constructor
+        writer.writeInt32(value.length);
+        for (const item of value) {
+          serializeSingleParam(writer, item, itemsType, note);
+        }
+
+        continue;
+      }
+      if (isTLObjectConstructor(type)) {
+        throw new Error("Unimplemented");
       }
 
-      switch (type) {
-        case "bigint":
-          if (typeof value === "bigint") {
-            if (note == "int128") {
-              writer.writeInt128(value);
-            } else if (note === "int256") {
-              writer.writeInt256(value);
-            } else {
-              writer.writeInt64(value);
-            }
-          } else {
-            throw new Error("Expected bigint");
-          }
-          break;
-        case "boolean":
-          if (typeof value === "boolean") {
-            if (value) {
-              writer.writeInt32(0x997275b5);
-            } else {
-              writer.writeInt32(0xbc799737);
-            }
-          } else {
-            throw new Error("Expected boolean");
-          }
-          break;
-        case "number":
-          if (typeof value === "number") {
-            writer.writeInt32(value);
-          } else {
-            throw new Error("Expected number");
-          }
-          break;
-        case "string":
-          if (typeof value === "string") {
-            writer.writeString(value);
-          } else if (value instanceof Uint8Array) {
-            writer.writeBytes(value);
-          } else {
-            throw new Error("Expected string or Uint8Array");
-          }
-          break;
-        case "true":
-          if (value === true) {
-            writer.writeInt32(0x997275b5);
-          } else {
-            throw new Error("Expected true");
-          }
-      }
+      serializeSingleParam(writer, value, type, note);
     }
 
     return writer.buffer;
