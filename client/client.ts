d895710866762e476495d877ea20843e8c07b72f
Roj <ez@roj.im>
2023-04-15 15:02:24 +0300

    Reject directly with TLObject

--- client/client.ts
+++ client/client.ts
@@ -14,11 +14,14 @@ import { ClientAbstract } from "./client_abstract.ts";
 import { ClientPlain } from "./client_plain.ts";
 
 export class Client extends ClientAbstract {
-  authKey?: Uint8Array;
-  authKeyId?: bigint;
-  salt?: bigint;
+  auth?: { key: Uint8Array; id: bigint };
+  salt = 0n;
+  seqNo = 0;
   sessionId: bigint;
-  seqno = 0;
+  promises = new Map<
+    bigint,
+    { resolve: (obj: TLObject) => void; reject: (err: TLObject) => void }
+  >();
 
   constructor(test?: boolean) {
     super(test);
@@ -30,15 +33,14 @@ export class Client extends ClientAbstract {
     await plain.connect();
     const { authKey, authKeyId, salt } = await plain.createAuthKey();
     await plain.disconnect();
-    this.authKey = authKey;
-    this.authKeyId = authKeyId;
+    this.auth = { key: authKey, id: authKeyId };
     this.salt = salt;
     await super.connect();
     this.loop();
   }
 
-  async loop() {
-    if (!this.authKey || !this.authKeyId) {
+  private async loop() {
+    if (!this.auth) {
       throw new Error("Not connected");
     }
 
@@ -46,8 +48,8 @@ export class Client extends ClientAbstract {
       const buffer = await this.transport.receive();
       const decrypted = await decryptMessage(
         buffer,
-        this.authKey,
-        this.authKeyId,
+        this.auth.key,
+        this.auth.id,
         this.sessionId,
       );
       const messages = decrypted instanceof MessageContainer
@@ -57,13 +59,12 @@ export class Client extends ClientAbstract {
       for (const message of messages) {
         if (message.body instanceof RPCResult) {
           const result = message.body.result;
-          const promise = this.promieMap.get(message.body.messageId);
+          const promise = this.promises.get(message.body.messageId);
           if (promise) {
-            const [resolve, reject] = promise;
             if (result instanceof RpcError) {
-              reject(new Error(`RPC error ${result.errorCode}`));
+              promise.reject(result);
             } else {
-              resolve(result);
+              promise.resolve(result);
             }
           }
         }
@@ -71,34 +72,27 @@ export class Client extends ClientAbstract {
     }
   }
 
-  promieMap = new Map<
-    bigint,
-    [(obj: TLObject) => void, (err: Error) => void]
-  >();
-
   async invoke(function_: Function) {
-    if (!this.authKey || !this.authKeyId || !this.salt) {
+    if (!this.auth) {
       throw new Error("Not connected");
     }
-    let seqNo = this.seqno * 2;
+    let seqNo = this.seqNo * 2;
     if (!(function_ instanceof Ping)) {
       seqNo++;
-      this.seqno++;
+      this.seqNo++;
     }
     const message = new Message(getMessageId(), seqNo, function_);
     await this.transport.send(
       await encryptMessage(
         message,
-        this.authKey,
-        this.authKeyId,
+        this.auth.key,
+        this.auth.id,
         this.salt,
         this.sessionId,
       ),
     );
-    console.log("sent");
-    // return null;
     return await new Promise<TLObject>((resolve, reject) => {
-      this.promieMap.set(message.id, [resolve, reject]);
+      this.promises.set(message.id, { resolve, reject });
     });
   }
 }
