2054f73d15b552fd3f7c07b04df2d19c70d111e8
Roj <ez@roj.im>
2023-04-15 18:41:17 +0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -5,9 +5,7 @@ export {
   assertFalse,
   assertInstanceOf,
   assertNotEquals,
-} from "https://deno.land/std@0.181.0/testing/asserts.ts";
-
-export { randomBigIntBits } from "https://deno.land/x/random_bigint@v1.5/src/randomBigInt.ts";
+} from "https://deno.land/std@0.183.0/testing/asserts.ts";
 
 export { factorize } from "https://deno.land/x/wasm_crypto@v0.3.0/mod.ts";
 
