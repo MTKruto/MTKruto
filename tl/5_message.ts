32c89581c5d224f1b04d262b6c18d49050bd1d4a
Roj <ez@roj.im>
2023-04-17 16:58:22 +0300

    Deserialize RPCResult in Message.deserialize

--- tl/5_message.ts
+++ tl/5_message.ts
@@ -1,4 +1,4 @@
-import { length, TLObject } from "./1_tl_object.ts";
+import { id, length, TLObject } from "./1_tl_object.ts";
 import { TLReader } from "./3_tl_reader.ts";
 import { TLWriter } from "./3_tl_writer.ts";
 import { RPCResult } from "./4_rpc_result.ts";
@@ -24,11 +24,19 @@ export class Message {
   }
 
   static deserialize(reader: TLReader) {
-    const id = reader.readInt64();
+    const id_ = reader.readInt64();
     const seqNo = reader.readInt32();
     const length = reader.readInt32();
     reader = new TLReader(reader.read(length));
-    const body = reader.readObject();
-    return new Message(id, seqNo, body);
+    const cid = reader.readInt32(false);
+    let body: TLObject | RPCResult;
+    {
+      if (cid == RPCResult[id]) {
+        body = RPCResult.deserialize(reader.buffer);
+      } else {
+        body = reader.readObject();
+      }
+    }
+    return new Message(id_, seqNo, body);
   }
 }
