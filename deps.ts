8cfce3ac7df846337f0ffcd4ebf7f7ab70f72a38
Roj <ez@roj.im>
2023-05-11 10:34:56 +0300

    Use the factorize function from tgcrypto_wasm

--- deps.ts
+++ deps.ts
@@ -2,9 +2,7 @@ export { assert, assertEquals, assertExists, assertFalse, assertInstanceOf, asse
 
 export * as log from "https://deno.land/std@0.186.0/log/mod.ts";
 
-export { factorize } from "https://deno.land/x/wasm_crypto@v0.3.0/mod.ts";
-
-export { ctr256Decrypt, ctr256Encrypt, ige256Decrypt, ige256Encrypt } from "https://deno.land/x/tgcrypto@0.0.6/mod.ts";
+export { ctr256Decrypt, ctr256Encrypt, ige256Decrypt, ige256Encrypt, factorize } from "https://deno.land/x/tgcrypto@0.0.6/mod.ts";
 
 export { gunzip, gzip } from "https://deno.land/x/compress@v0.4.5/zlib/mod.ts";
 
