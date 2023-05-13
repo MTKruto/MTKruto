c87ce4617ba03fb62d4b867a1dc586e985904b0f
Roj <ez@roj.im>
2023-05-13 13:00:02 +0300

    TLRawReader.readInt64

--- tl/0_tl_raw_reader_test.ts
+++ tl/0_tl_raw_reader_test.ts
@@ -2,7 +2,16 @@ import { assertEquals } from "../deps_t.ts";
 import { TLRawReader } from "./0_tl_raw_reader.ts";
 
 Deno.test("TLRawReader", async (t) => {
-  const buffer = new Uint8Array([0x00, 0xff, 0xff, 0xff, 0x01, 0x00, 0x80, 0xcc, 0xee, 0xff, 0xff, 0xff, 0xff, 0xfe, 0xff]);
+  // deno-fmt-ignore
+  const buffer = new Uint8Array([
+    0x00, // read 1
+    0xff, 0xff, 0xff, // uint24
+    0x01, 0x00, 0x80, // int24
+    0xcc, 0xee, 0xff, 0xff, // uint32
+    0xff, 0xff, 0xfe, 0xff, // int32
+    0x68, 0xff, 0x98, 0x88, 0xdd, 0xcc, 0xff, 0xee, // uint64
+    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 // int64
+  ]);
   const reader = new TLRawReader(buffer);
 
   await t.step("read", () => {
@@ -17,5 +26,10 @@ Deno.test("TLRawReader", async (t) => {
   await t.step("readInt32", () => {
     assertEquals(reader.readInt32(false), 0xff_ff_ee_cc);
     assertEquals(reader.readInt32(), -0x010001);
+  });
+
+  await t.step("readInt64", () => {
+    assertEquals(reader.readInt64(false), 17221708751939633000n);
+    assertEquals(reader.readInt64(), -9223372036854775807n);
   });
 });
