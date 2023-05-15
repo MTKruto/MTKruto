e4b61dbbc74b1e8ed61a60d37a5f18d2f28f7516
Roj <ez@roj.im>
2023-05-15 11:06:02 +0300

    Update tgcrypto

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export * from "https://deno.land/std@0.186.0/testing/asserts.ts";
 
 export * as log from "https://deno.land/std@0.186.0/log/mod.ts";
 
-export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.0.9/mod.ts";
+export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.0/mod.ts";
 
 export { gunzip, gzip } from "https://deno.land/x/compress@v0.4.5/zlib/mod.ts";
 
