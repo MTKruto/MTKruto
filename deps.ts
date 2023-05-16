74761b2f4ca2a5ddf199a015b0f61f8942bf8db7
Roj <ez@roj.im>
2023-05-16 12:51:36 +0300

    Change import

--- deps.ts
+++ deps.ts
@@ -1,8 +1,7 @@
 export * from "https://deno.land/std@0.186.0/testing/asserts.ts";
 
-
 export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.1/mod.ts";
 
-export { gunzip, gzip } from "https://deno.land/x/compress@v0.4.5/zlib/mod.ts";
+export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/master/gzip/gzip.ts";
 
 export { Mutex } from "https://esm.sh/async-mutex@0.4.0";
