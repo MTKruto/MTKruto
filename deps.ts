ef45662d2267ab84ba11646f9006785bc651d3bd
Roj <ez@roj.im>
2023-05-16 22:14:55 +0300

    Bump tgcrypto

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * from "https://deno.land/std@0.186.0/testing/asserts.ts";
 
-export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.1/mod.ts";
+export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.2/mod.ts";
 
 export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/master/gzip/gzip.ts";
 
