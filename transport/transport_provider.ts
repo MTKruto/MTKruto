25db42729f75eac9387183a5e64fce96223355cc
Roj <ez@roj.im>
2023-05-17 21:21:09 +0300

    Implement sessions (#11)

--- transport/transport_provider.ts
+++ transport/transport_provider.ts
@@ -5,9 +5,10 @@ import { TransportIntermediate } from "./transport_intermediate.ts";
 
 export interface TransportProviderParams {
   dc: "1" | "2" | "3" | "4" | "5" | "1-test" | "2-test" | "3-test";
+  cdn: boolean;
 }
 
-export type TransportProvider = (params?: TransportProviderParams) => (cdn: boolean) => { connection: Connection; transport: Transport; dcId: number };
+export type TransportProvider = (params: TransportProviderParams) => { connection: Connection; transport: Transport; dcId: number };
 
 export const defaultDc: TransportProviderParams["dc"] = "2-test";
 const dcToNameMap: Record<TransportProviderParams["dc"], string> = {
@@ -20,13 +21,13 @@ const dcToNameMap: Record<TransportProviderParams["dc"], string> = {
   "4": "vesta",
   "5": "flora",
 };
-export const defaultTransportProvider: TransportProvider = (params?: TransportProviderParams & { wss?: boolean }) => {
-  return (cdn) => {
-    const { dc, wss = typeof location !== "undefined" && location.protocol == "http:" ? false : true } = params ?? { dc: defaultDc };
+export function defaultTransportProvider(wss?: boolean): TransportProvider {
+  return ({ dc, cdn }) => {
+    wss ??= typeof location !== "undefined" && location.protocol == "http:" ? false : true;
     const url = `${wss ? "wss" : "ws"}://${dcToNameMap[dc]}${cdn ? "-1" : ""}.web.telegram.org/${dc.endsWith("-test") ? "apiws_test" : "apiws"}`;
     const connection = new ConnectionWebSocket(url);
     const transport = new TransportIntermediate(connection, true);
     const dcId = Number(dc[0]) + (dc.endsWith("-test") ? 10_000 : 0) * (cdn ? -1 : 1);
     return { connection, transport, dcId };
   };
-};
+}
