2aac673bb18942a2b41c4af6fddd252335896e04
Roj <ez@roj.im>
2023-05-17 14:43:43 +0300

    Re-export more symbols from mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,14 @@
+import { checkPassword } from "./utilities/1_password.ts";
+import { getRandomId } from "./utilities/0_bigint.ts";
+
+export const utils = { checkPassword, getRandomId };
+
 export * as types from "./tl/2_types.ts";
 export * as functions from "./tl/3_functions.ts";
-export { ClientPlain } from "./client/client_plain.ts";
-export { Client } from "./client/client.ts";
+export * from "./client/client_plain.ts";
+export * from "./client/client.ts";
+export * from "./transport/transport_abridged.ts";
+export * from "./transport/transport_intermediate.ts";
+export * from "./transport/transport.ts";
+export * from "./transport/transport_provider.ts";
+export * from "./connection/connection.ts";
