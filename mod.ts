8c78654a98fa5a0a9883659bb6e66c8402f3ca07
Roj <ez@roj.im>
2023-05-18 10:08:23 +0300

    Re-export more symbols (#15)

--- mod.ts
+++ mod.ts
@@ -5,6 +5,9 @@ export const utils = { checkPassword, getRandomId };
 
 export * as types from "./tl/2_types.ts";
 export * as functions from "./tl/3_functions.ts";
+export * from "./tl/4_rpc_result.ts";
+export * from "./tl/5_message.ts";
+export * from "./tl/6_message_container.ts";
 export * from "./client/client_plain.ts";
 export * from "./client/client.ts";
 export * from "./session/session.ts";
@@ -15,3 +18,4 @@ export * from "./transport/transport_intermediate.ts";
 export * from "./transport/transport.ts";
 export * from "./transport/transport_provider.ts";
 export * from "./connection/connection.ts";
+export * from "./connection/connection_web_socket.ts";
