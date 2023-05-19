78567541f29c26073ad97124bfe64a6655cadefe
Roj <ez@roj.im>
2023-05-19 14:10:13 +0300

    Support serializing and deserializing vectors directly
    Closes #17.

--- tl/3_tl_reader.ts
+++ tl/3_tl_reader.ts
@@ -1,12 +1,22 @@
 import { TLRawReader } from "./0_tl_raw_reader.ts";
-import { paramDesc } from "./1_tl_object.ts";
+import { paramDesc, TLObject } from "./1_tl_object.ts";
 import { map } from "./2_types.ts";
 import { deserialize } from "./3_tl_object_deserializer.ts";
 
+export type ReadObject = TLObject | Array<ReadObject | TLObject>;
+
 export class TLReader extends TLRawReader {
-  readObject(id?: number) {
+  readObject(id?: number): ReadObject {
     if (!id) {
       id = this.readInt32(false);
+    }
+    if (id == 0x1CB5C415) {
+      const count = this.readInt32();
+      const items = new Array<ReadObject>();
+      for (let i = 0; i < count; i++) {
+        items.push(this.readObject());
+      }
+      return items;
     }
     const constructor = map.get(id);
     if (constructor) {
--- tl/3_tl_writer.ts
+++ tl/3_tl_writer.ts
@@ -1,9 +1,18 @@
+import { VECTOR_CONSTRUCTOR } from "../constants.ts";
 import { TLRawWriter } from "./0_tl_raw_writer.ts";
-import { TLObject } from "./1_tl_object.ts";
+import { MaybeVectorTLObject } from "./1_tl_object.ts";
 
 export class TLWriter extends TLRawWriter {
-  writeObject(object: TLObject) {
-    this.write(object.serialize());
+  writeObject(object: MaybeVectorTLObject) {
+    if (Array.isArray(object)) {
+      this.writeInt32(VECTOR_CONSTRUCTOR, false);
+      this.writeInt32(object.length);
+      for (const item of object) {
+        this.writeObject(item);
+      }
+    } else {
+      this.write(object.serialize());
+    }
     return this;
   }
 }
