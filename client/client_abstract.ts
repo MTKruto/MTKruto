d2a831c17ff199d5d966416d40444a59e9104fbe
Roj <ez@roj.im>
2023-05-19 20:30:36 +0300

    Implement `Client.authorize()` for bots
    - Fix type of `Session.dc`.
    - Make `dcId` a private property of `ClientAbstract` named `_dcId` that will only be changed from `setDc()` — convert `dcId` to a getter.
    - Add the method `ClientAbstract.reconnect()` that reconnects the connection, optionally with a new DC.
    - Add optional `apiId` and `apiHash` parameters to `Client`’s constructor.
    - Add the method `Client.authorize()` that calls `InitConnection`, and imports bot authorization if not already authorized.

--- client/client_abstract.ts
+++ client/client_abstract.ts
@@ -2,19 +2,30 @@ import { initTgCrypto } from "../deps.ts";
 import { DEFAULT_INITIAL_DC } from "../constants.ts";
 import { Connection } from "../connection/connection.ts";
 import { Transport } from "../transport/transport.ts";
-import { defaultTransportProvider } from "../transport/transport_provider.ts";
+import { DC, defaultTransportProvider } from "../transport/transport_provider.ts";
 
 export abstract class ClientAbstract {
   protected connection: Connection;
   protected transport: Transport;
-  dcId: number;
+  private _dcId: number;
   protected connected = false;
 
   constructor(protected transportProvider = defaultTransportProvider({ initialDc: DEFAULT_INITIAL_DC })) {
     const { connection, transport, dcId } = transportProvider({ cdn: false });
     this.connection = connection;
     this.transport = transport;
-    this.dcId = dcId;
+    this._dcId = dcId;
+  }
+
+  get dcId() {
+    return this._dcId;
+  }
+
+  setDc(dc: DC) {
+    const { connection, transport, dcId } = this.transportProvider({ dc, cdn: false });
+    this.connection = connection;
+    this.transport = transport;
+    this._dcId = dcId;
   }
 
   async connect() {
@@ -22,6 +33,14 @@ export abstract class ClientAbstract {
     await this.connection.open();
     await this.transport.initialize();
     this.connected = true;
+  }
+
+  async reconnect(dc?: DC) {
+    await this.disconnect();
+    if (dc) {
+      this.setDc(dc);
+    }
+    await this.connect();
   }
 
   async disconnect() {
