3341f356f629bab0650760997f7c2588c5d1106b
Roj <ez@roj.im>
2023-05-21 18:07:03 +0300

    Compute authKeyId in Session

--- session/session.ts
+++ session/session.ts
@@ -1,9 +1,29 @@
 import { MaybePromise } from "../types.ts";
 import { DC } from "../transport/transport_provider.ts";
+import { sha1 } from "../utilities/0_hash.ts";
+import { bigIntFromBuffer } from "../utilities/0_bigint.ts";
 
 export abstract class Session {
   dc: DC | null = null;
-  authKey: Uint8Array | null = null;
+  private _authKeyId: Promise<bigint | null> = Promise.resolve(null);
+  private _authKey: Uint8Array | null = null;
+
   abstract load(): MaybePromise<void>;
   abstract save(): MaybePromise<void>;
+
+  get authKeyId() {
+    return this._authKeyId;
+  }
+
+  set authKey(authKey: Uint8Array | null) {
+    this._authKey = authKey;
+
+    if (authKey != null) {
+      this._authKeyId = sha1(authKey).then((hash) => bigIntFromBuffer(hash.slice(-8), true, false));
+    }
+  }
+
+  get authKey() {
+    return this._authKey;
+  }
 }
