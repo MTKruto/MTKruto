fcd2e5b244d9b3165cb4aa3acf6772eb954e309b
Roj <ez@roj.im>
2023-05-29 22:33:45 +0300

    Add more properties to Message

--- types/1_message.ts
+++ types/1_message.ts
@@ -1,5 +1,7 @@
 import * as types from "../tl/2_types.ts";
+import { Chat } from "./0_chat.ts";
 import { fromTlObject, MessageEntity } from "./0_message_entity.ts";
+import { User } from "./0_user.ts";
 
 export class Message {
   id: number;
@@ -10,9 +12,47 @@ export class Message {
   date: Date;
   editDate: Date | null = null;
   views: number | null = null;
+  from?: User;
+  chat?: Chat;
 
-  constructor(message: types.Message) {
+  constructor(message: types.Message, users: types.TypeUser[], chats: types.TypeChat[]) {
     this.id = message.id;
+    if (message.fromId instanceof types.PeerUser) {
+      for (const user of users) {
+        if (user instanceof types.User && user.id == message.fromId.userId) {
+          this.from = new User(user);
+          break;
+        }
+      }
+    }
+    let chat_: Chat | null = null;
+    if (message.peerId instanceof types.PeerUser) {
+      for (const user of users) {
+        if (user instanceof types.User && user.id == message.peerId.userId) {
+          chat_ = new Chat(user);
+          break;
+        }
+      }
+    } else if (message.peerId instanceof types.PeerChat) {
+      for (const chat of chats) {
+        if (chat instanceof types.Chat && chat.id == message.peerId.chatId) {
+          chat_ = new Chat(chat);
+          break;
+        }
+      }
+    } else if (message.peerId instanceof types.PeerChannel) {
+      for (const chat of chats) {
+        if (chat instanceof types.Channel && chat.id == message.peerId.channelId) {
+          chat_ = new Chat(chat);
+          break;
+        }
+      }
+    }
+    if (!chat_) {
+      throw new Error("Unreachable");
+    } else {
+      this.chat = chat_;
+    }
     if (message.message) {
       if (message.media == undefined) {
         this.text = message.message;
