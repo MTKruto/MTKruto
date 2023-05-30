f6caaaa37e3e4513ad9168ae6095f5950b29dd34
Roj <ez@roj.im>
2023-05-30 12:05:06 +0000

    Convert types to interfaces and extend them (#31)

---
+++ types/1_user.ts
@@ -0,0 +1,47 @@
+import { as } from "../tl/1_tl_object.ts";
+import * as types from "../tl/2_types.ts";
+import { ChatPhotoUser, constructChatPhoto } from "./0_chat_photo.ts";
+
+export interface User {
+  id: number;
+  isBot: boolean;
+  firstName: string;
+  lastName?: string;
+  username?: string;
+  also?: string[];
+  photo?: ChatPhotoUser;
+  languageCode?: string;
+  isScam: boolean;
+  isFake: boolean;
+  isPremium: boolean;
+  isVerified: boolean;
+  isSupport: boolean;
+  addedToAttachmentMenu: boolean;
+}
+
+export function constructUser(user_: types.User) {
+  const user: User = {
+    id: Number(user_.id),
+    isBot: user_.bot || false,
+    firstName: user_.firstName || "",
+    lastName: user_.lastName,
+    username: user_.username,
+    languageCode: user_.langCode,
+    isScam: user_.scam || false,
+    isFake: user_.fake || false,
+    isPremium: user_.premium || false,
+    isVerified: user_.verified || false,
+    isSupport: user_.support || false,
+    addedToAttachmentMenu: user_.attachMenuEnabled || false,
+  };
+
+  if (user_.usernames) {
+    user.also = user_.usernames.map((v) => v[as](types.Username)).map((v) => v.username);
+  }
+
+  if (user_.photo instanceof types.UserProfilePhoto) {
+    user.photo = constructChatPhoto(user_.photo);
+  }
+
+  return user;
+}
