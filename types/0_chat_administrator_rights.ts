fe2f4f86844f5c18ee4593b16c8e359376fe5d21
Roj <rojvv@icloud.com>
2023-06-01 10:23:31 +0000

    Add reply markup types (#32)

---
+++ types/0_chat_administrator_rights.ts
@@ -0,0 +1,44 @@
+import * as types from "../tl/2_types.ts";
+
+export interface ChatAdministratorRights {
+  isAnonymous: boolean;
+  canManageChat: boolean;
+  canDeleteMessages: boolean;
+  canManageVideoChats: boolean;
+  canRestrictMembers: boolean;
+  canPromoteMembers: boolean;
+  canChangeInfo: boolean;
+  canInviteUsers: boolean;
+  canPostMessages?: boolean;
+  canEditMessages?: boolean;
+  canPinMessages?: boolean;
+  canManageTopics?: boolean;
+}
+
+export function constructChatAdministratorRights(rights_: types.ChatAdminRights) {
+  const rights: ChatAdministratorRights = {
+    isAnonymous: rights_.anonymous || false,
+    canManageChat: rights_.other || false,
+    canDeleteMessages: rights_.deleteMessages || false,
+    canManageVideoChats: rights_.manageCall || false,
+    canRestrictMembers: rights_.banUsers || false,
+    canPromoteMembers: rights_.addAdmins || false,
+    canChangeInfo: rights_.changeInfo || false,
+    canInviteUsers: rights_.inviteUsers || false,
+  };
+
+  if (rights_.postMessages) {
+    rights.canPostMessages = rights_.postMessages;
+  }
+  if (rights_.editMessages) {
+    rights.canEditMessages = rights_.editMessages;
+  }
+  if (rights_.pinMessages) {
+    rights.canPinMessages = rights_.pinMessages;
+  }
+  if (rights_.manageTopics) {
+    rights.canManageTopics = rights_.manageTopics;
+  }
+
+  return rights;
+}
