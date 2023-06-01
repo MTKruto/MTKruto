829db5df0c97f021dde1692548c839542b866704
Roj <ez@roj.im>
2023-06-01 16:23:34 +0300

    Fix MTKruto/compress import

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export * from "https://deno.land/std@0.190.0/testing/asserts.ts";
 
 export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.3/mod.ts";
 
-export { gunzip, gzip } from "https://esm.sh/gh/MTKruto/compress@master/gzip/gzip.ts";
+export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/master/gzip/gzip.ts";
 
 export { Mutex } from "https://esm.sh/async-mutex@0.4.0";
 
