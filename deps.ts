cd2c54113a549ea9022fcca4c3b919e8b8e04d73
Roj <ez@roj.im>
2023-06-07 10:24:40 +0300

    Remove unused dependency and fix import

--- deps.ts
+++ deps.ts
@@ -4,14 +4,12 @@ export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, 
 
 export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/master/gzip/gzip.ts";
 
-export { Mutex } from "https://esm.sh/async-mutex@0.4.0";
+export { Mutex, type MutexInterface } from "https://esm.sh/async-mutex@0.4.0";
 
 export { Parser } from "https://deno.land/x/html_parser@v0.1.3/src/mod.ts";
 
 import { debug as debug_ } from "https://raw.githubusercontent.com/MTKruto/debug/master/mod.ts";
 
 export const debug: typeof debug_ = (v) => debug_(`mtkruto/${v}`);
-
-export { queue } from "https://deno.land/x/q@v0.0.1/mod.ts";
 
 export { decode as base64Decode, encode as base64Encode } from "https://deno.land/std@0.190.0/encoding/base64.ts";
