76d3ea27c4f99e5c3263af4743647366f960485e
Roj <ez@roj.im>
2023-08-23 20:35:58 +0300

    Change branches

--- deps.ts
+++ deps.ts
@@ -2,13 +2,13 @@ export * from "https://deno.land/std@0.199.0/assert/mod.ts";
 
 export { ctr256Decrypt, ctr256Encrypt, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto } from "https://deno.land/x/tgcrypto@0.1.3/mod.ts";
 
-export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/master/gzip/gzip.ts";
+export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/main/gzip/gzip.ts";
 
 export { Mutex, type MutexInterface } from "https://esm.sh/async-mutex@0.4.0";
 
 export { Parser } from "https://deno.land/x/html_parser@v0.1.3/src/mod.ts";
 
-import { debug as debug_ } from "https://raw.githubusercontent.com/MTKruto/debug/master/mod.ts";
+import { debug as debug_ } from "https://raw.githubusercontent.com/MTKruto/debug/main/mod.ts";
 
 export const debug: typeof debug_ = (v) => debug_(`mtkruto/${v}`);
 
