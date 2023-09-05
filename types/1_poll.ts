cbac911266c85d8cafe7f683bc0fc5881ab72236
Roj <ez@roj.im>
2023-09-05 18:27:59 +0300

    Improve consistency

--- types/1_poll.ts
+++ types/1_poll.ts
@@ -20,15 +20,15 @@ export interface Poll {
   type: "regular" | "quiz";
   /** True, if the poll allows multiple answers */
   allowMultipleAnswers?: boolean;
-  /** Optional. 0-based identifier of the correct answer option. Available only for polls in the quiz mode, which are closed, or was sent (not forwarded) by the bot or to the private chat with the bot. */
+  /**  0-based identifier of the correct answer option. Available only for polls in the quiz mode, which are closed, or was sent (not forwarded) by the bot or to the private chat with the bot. */
   correctOptionId?: number;
-  /** Optional. Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters */
+  /** Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters */
   explanation?: string;
-  /** Optional. Special entities like usernames, URLs, bot commands, etc. that appear in the explanation */
+  /** Special entities like usernames, URLs, bot commands, etc. that appear in the explanation */
   explanationEntities?: MessageEntity[];
-  /** Optional. Amount of time in seconds the poll will be active after creation */
+  /** Amount of time in seconds the poll will be active after creation */
   openPeriod?: number;
-  /** Optional. Point in time (Unix timestamp) when the poll will be automatically closed */
+  /** Point in time (Unix timestamp) when the poll will be automatically closed */
   closeDate?: Date;
 }
 
