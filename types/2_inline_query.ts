62643b702bfe736dae5b33edca77d9e104eb14e3
Roj <ez@roj.im>
2023-09-06 11:51:02 +0300

    Implement InlineQuery and inlineQuery updates

---
+++ types/2_inline_query.ts
@@ -0,0 +1,57 @@
+import * as types from "../tl/2_types.ts";
+import { constructUser, User } from "./1_user.ts";
+import { constructLocation, Location } from "./0_location.ts";
+import { EntityGetter } from "./!0_misc.ts";
+import { UNREACHABLE } from "../utilities/0_control.ts";
+
+export interface InlineQuery {
+  /** Unique identifier for this query */
+  id: string;
+  /** The user who made this query */
+  from: User;
+  /** The text of the query */
+  query: string;
+  /** Result offset */
+  offset: string;
+  /** Type of the chat from which the inline query was sent. Can be either “sender” for a private chat with the inline query sender, “private”, “group”, “supergroup”, or “channel”. The chat type should be always known for requests sent from official clients and most third-party clients, unless the request was sent from a secret chat */
+  chatType?: "sender" | "private" | "group" | "supergroup" | "channel";
+  /** The location of the user who made this query */
+  location?: Location;
+}
+
+export async function constructInlineQuery(query_: types.UpdateBotInlineQuery, getEntity: EntityGetter): Promise<InlineQuery> {
+  const user_ = await getEntity(new types.PeerUser({ userId: query_.userId }));
+  if (user_ == null) {
+    UNREACHABLE();
+  }
+
+  const user = constructUser(user_);
+
+  let chatType: InlineQuery["chatType"] | undefined;
+  if (query_.peerType !== undefined) {
+    if (query_.peerType instanceof types.InlineQueryPeerTypeSameBotPM) {
+      chatType = "private";
+    } else if (query_.peerType instanceof types.InlineQueryPeerTypeBotPM || query_.peerType instanceof types.InlineQueryPeerTypePM) {
+      chatType = "sender";
+    } else if (query_.peerType instanceof types.InlineQueryPeerTypeChat) {
+      chatType = "group";
+    } else if (query_.peerType instanceof types.InlineQueryPeerTypeMegagroup) {
+      chatType = "supergroup";
+    } else if (query_.peerType instanceof types.InlineQueryPeerTypeBroadcast) {
+      chatType = "channel";
+    } else {
+      UNREACHABLE();
+    }
+  }
+
+  const location = query_.geo !== undefined && query_.geo instanceof types.GeoPoint ? constructLocation(query_.geo) : undefined;
+
+  return {
+    id: String(query_.queryId),
+    from: user,
+    query: query_.query,
+    offset: query_.offset,
+    chatType,
+    location,
+  };
+}
