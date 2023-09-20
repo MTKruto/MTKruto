07a10ec96569a435e00c4a130a16c31d25015a26
Roj <ez@roj.im>
2023-09-20 22:39:54 +0300

    Update tgcrypto

--- utilities/0_crypto.ts
+++ utilities/0_crypto.ts
@@ -1,4 +1,4 @@
-import { ctr256Decrypt, ctr256Encrypt } from "../0_deps.ts";
+import { ctr256 } from "../0_deps.ts";
 
 export class CTR {
   state = new Uint8Array(1);
@@ -6,29 +6,9 @@ export class CTR {
   constructor(public readonly key: Uint8Array, public iv: Uint8Array) {
   }
 
-  encrypt(data: Uint8Array) {
+  call(data: Uint8Array) {
     const v = new Uint8Array(data); // TODO: don't copy
-    const [iv, state] = ctr256Encrypt(
-      v,
-      this.key,
-      this.iv,
-      this.state,
-    );
-    this.iv = iv;
-    this.state = state;
-    return v;
-  }
-
-  decrypt(data: Uint8Array) {
-    const v = new Uint8Array(data); // TODO: don't copy
-    const [iv, state] = ctr256Decrypt(
-      v,
-      this.key,
-      this.iv,
-      this.state,
-    );
-    this.iv = iv;
-    this.state = state;
+    ctr256(v, this.key, this.iv, this.state);
     return v;
   }
 }
