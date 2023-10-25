6ca92c43576aa2cfc2c590ad8f6a94fd7f926388
Roj <ez@roj.im>
2023-10-25 19:23:43 +0300

    InputContactMessageContent

--- types/0_input_contact_message_content.ts
+++ types/0_input_contact_message_content.ts
@@ -1,6 +1,11 @@
+/** A message content that shares a contact. */
 export interface InputContactMessageContent {
+  /** The contact's phone number. */
   phoneNumber: string;
+  /** The contact's first name. */
   firstName: string;
+  /** The contact's last name. */
   lastName?: string;
+  /** Additional information in the vCard format. */
   vcard?: string;
 }
