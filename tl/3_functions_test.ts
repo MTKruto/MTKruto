79bdee9fbb4994a7dda0c8a53346ed7b97140210
Roj <ez@roj.im>
2023-12-01 22:35:43 +0300

    Fix test

--- tl/3_functions_test.ts
+++ tl/3_functions_test.ts
@@ -1,6 +1,6 @@
 import { assert } from "../0_deps.ts";
 import { TLObject } from "./1_tl_object.ts";
-import { Function, ping } from "./4_functions.ts";
+import { Function, ping } from "./3_functions.ts";
 
 Deno.test("instanceof", () => {
   const ping_ = new ping({ ping_id: 0xFFn });
