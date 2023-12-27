6393421c56eb3c7612860937b54258585fa099fa
TheBleedingEdge <thebleedingedge@yahoo.com>
2023-12-27 23:49:02 +0530

    added description to fields, changed doc structure

--- client/3_types.ts
+++ client/3_types.ts
@@ -53,13 +53,21 @@ export interface ClientParams extends ClientPlainParams {
 }
 
 export interface AnswerCallbackQueryParams {
-  /** A text to be shown to the user. */
+  /**
+   * A text to be shown to the user.
+   */
   text?: string;
-  /** Whether to show the text as an alert (popup). */
+  /**
+   * Whether to show the text as an alert (popup).
+   */
   alert?: boolean;
-  /** A URL to be opened. */
+  /**
+   * A URL to be opened.
+   */
   url?: string;
-  /** TTL of answer caches in seconds. */
+  /**
+   * TTL of answer caches in seconds.
+   */
   cacheTime?: number;
 }
 
@@ -188,56 +196,99 @@ export interface SendPollParams extends _SendCommon {
 }
 
 export interface DownloadParams {
-  /** Size of each download chunk in bytes. */
+  /**
+   * Size of each download chunk in bytes.
+   */
   chunkSize?: number;
 }
 
 export interface _UploadCommon {
-  /** The file name to assign if applicable. */
+  /**
+   * The file name to assign if applicable.
+   */
   fileName?: string;
-  /** The mime type to assign if applicable. */
+  /**
+   * The mime type to assign if applicable.
+   */
   mimeType?: string;
-  /** Size of each upload chunk in bytes. */
+  /**
+   * Size of each upload chunk in bytes.
+   */
   chunkSize?: number;
-  /** Upload abort signal. */
+  /**
+   * Upload abort signal.
+   */
   signal?: AbortSignal | null;
 }
 export interface UploadParams {
-  /** The file name to assign. */
+  /**
+   * The file name to assign.
+   */
   fileName?: string;
-  /** Size of each upload chunk in bytes. */
+  /**
+   * Size of each upload chunk in bytes.
+   */
   chunkSize?: number;
-  /** Upload abort signal. */
+  /**
+   * Upload abort signal.
+   */
   signal?: AbortSignal | null;
 }
 
 export interface AnswerInlineQueryParams {
+  /**
+   * The maximum amount of time in seconds to cache the result on server, Defaults to 300.
+   */
   cacheTime?: number;
+  /**
+   * Whether the results may be cached on the server side only for the user that sent the query.
+   */
   isPersonal?: boolean;
+  /**
+   * Pass the offset that a client should send in the next query with the same text to receive more results. Pass an empty string if there are no more results or if you don't support pagination. Offset length can't exceed 64 bytes.
+   */
   nextOffset?: string;
+  /**
+   * 
+   */
   isGallery?: boolean;
+  /**
+   * A JSON-serialized object describing a button to be shown above inline query results.
+   */
   button?: InlineQueryResultButton;
 }
 
 export interface SetMyCommandsParams {
+  /**
+   * A two-letter ISO 639-1 language code. If empty, commands will be applied to all users from the given scope, for whose language there are no dedicated commands.
+   */
   languageCode?: string;
+  /**
+   * A JSON-serialized object, describing scope of users for which the commands are relevant. Defaults to BotCommandScopeDefault.
+   */
   scope?: BotCommandScope;
 }
 
 export type GetMyCommandsParams = SetMyCommandsParams;
 
 export interface DeleteMessagesParams {
-  /** Whether to delete the messages only for this side. */
+  /**
+   * Whether to delete the messages only for this side.
+   */
   onlyForMe?: boolean;
 }
 
 export interface DeleteMessageParams {
-  /** Whether to delete the message only for this side. */
+  /**
+   * Whether to delete the message only for this side.
+   */
   onlyForMe?: boolean;
 }
 
 export interface _CaptionCommon {
-  /** The caption to attach. */
+  /**
+   * The caption to attach.
+   */
   caption?: string;
   /**
    * The caption's entities.
@@ -258,68 +309,128 @@ export interface SendPhotoParams extends _CaptionCommon, _SpoilCommon, _UploadCo
 }
 
 export interface _ThumbnailCommon {
-  /** A thumbnail to assign. Cannot be a URL. */
+  /**
+   * A thumbnail to assign. Cannot be a URL.
+   */
   thumbnail?: FileSource;
 }
 export interface SendDocumentParams extends _CaptionCommon, _ThumbnailCommon, _SpoilCommon, _UploadCommon, _SendCommon {
 }
 
 export interface SendVideoParams extends _CaptionCommon, _ThumbnailCommon, _SpoilCommon, _UploadCommon, _SendCommon {
-  /** The duration of the video in seconds. */
+  /**
+   * The duration of the video in seconds.
+   */
   duration?: number;
-  /** The width of the photo in pixels. */
+  /**
+   * The width of the photo in pixels.
+   */
   width?: number;
-  /** The height of the photo in pixels. */
+  /**
+   * The height of the photo in pixels.
+   */
   height?: number;
+  /**
+   * Pass True if the uploaded video is suitable for streaming.
+   */
   supportsStreaming?: boolean;
 }
 
 export interface SendAnimationParams extends _CaptionCommon, _ThumbnailCommon, _SpoilCommon, _UploadCommon, _SendCommon {
-  /** The duration of the animation in seconds. */
+  /**
+   * The duration of the animation in seconds.
+   */
   duration?: number;
+  /**
+   * Animation width.
+   */
   width?: number;
+  /**
+   * Animation height.
+   */
   height?: number;
 }
 
 export interface SendVoiceParams extends _CaptionCommon, _ThumbnailCommon, _UploadCommon, _SendCommon {
-  /** The duration of the voice message in seconds. */
+  /** 
+   * he duration of the voice message in seconds.
+   */
   duration?: number;
 }
 
 export interface SendAudioParams extends _CaptionCommon, _ThumbnailCommon, _UploadCommon, _SendCommon {
-  /** The duration of the audio file in seconds. */
+  /**
+   * The duration of the audio file in seconds.
+   */
   duration?: number;
+  /**
+   * Performer.
+   */
   performer?: string;
+  /**
+   * Track name.
+   */
   title?: string;
 }
 
 export interface SendVideoNoteParams extends _CaptionCommon, _ThumbnailCommon, _UploadCommon, _SendCommon {
   /** The duration of the video note in seconds. */
   duration?: number;
+  /**
+   * Video width and height, i.e. diameter of the video message.
+   */
   length?: number;
+  /**
+   * Thumbnail of the file sent, should be in JPEG format and less than 200 kB in size. The height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. 
+   */
   thumbnail?: FileSource;
 }
 
 export interface SendLocationParams extends _SendCommon {
+  /**
+   * The radius of uncertainty for the location, measured in meters; 0-1500.
+   */
   horizontalAccuracy?: number;
+  /**
+   * Period in seconds for which the location will be updated, should be between 60 and 86400.
+   */
   livePeriod?: number;
+  /**
+   * For live locations, a direction in which the user is moving, in degrees. Must be between 1 and 360 if specified.
+   */
   heading?: number;
+  /**
+   * For live locations, a maximum distance for proximity alerts about approaching another chat member, in meters. Must be between 1 and 100000 if specified.
+   */
   proximityAlertRadius?: number;
 }
 
 export interface SendVenueParams extends _SendCommon {
+  /**
+   * Foursquare identifier of the venue.
+   */
   foursquareId?: string;
+  /**
+   * Foursquare type of the venue, if known. (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
+   */
   foursquareType?: string;
 }
 
 export interface SendContactParams extends _SendCommon {
-  /** The contact's last name. */
+  /**
+   * The contact's last name.
+   */
   lastName?: string;
-  /** Additional information in the vCard format. */
+  /**
+   * Additional information in the vCard format.
+   */
   vcard?: string;
 }
 
 export interface SendDiceParams extends _SendCommon {
+  /**
+   * Emoji on which the dice throw animation is based. Currently, must be one of “🎲”, “🎯”, “🏀”, “⚽”, “🎳”, or “🎰”. Dice can have values 1-6 for “🎲”, “🎯” and “🎳”, values 1-5 for “🏀” and “⚽”, and values 1-64 for “🎰”. Defaults to “🎲”.
+   */
   emoji?: "🎲" | "🎯" | "🏀" | "⚽" | "🎳" | "🎰";
 }
 
@@ -328,14 +439,20 @@ export type ConnectionState = "notConnected" | "updating" | "ready";
 export type AuthorizationState = { authorized: boolean };
 
 export interface ReplyParams {
-  /** Whether to quote the message that is to be replied. Enabled by default for non-private chats. */
+  /**
+   * Whether to quote the message that is to be replied. Enabled by default for non-private chats.
+   */
   quote?: boolean;
 }
 
 export interface GetHistoryParams {
-  /** The oldest message to get messages after. */
+  /**
+   * The oldest message to get messages after.
+   */
   after?: Message;
-  /** The maximum number of results to return. Must be in the range of 1-100. Defaults to 100. */
+  /**
+   * The maximum number of results to return. Must be in the range of 1-100. Defaults to 100.
+   */
   limit?: number;
 }
 
@@ -347,13 +464,28 @@ export interface AddReactionParams {
 export type MessageUpdates = "message" | "editedMessage";
 
 export interface Update {
+  /**
+   * New incoming message of any kind - text, photo, sticker, etc.
+   */
   message?: Message;
+  /**
+   * New version of a message that is known to the bot and was edited.
+   */
   editedMessage?: Message;
   connectionState?: ConnectionState;
   authorizationState?: AuthorizationState;
   deletedMessages?: [Message, ...Message[]];
+  /**
+   * New incoming callback query.
+   */
   callbackQuery?: CallbackQuery;
+  /**
+   * New incoming inline query.
+   */
   inlineQuery?: InlineQuery;
+  /**
+   * The result of an inline query that was chosen by a user and sent to their chat partner.
+   */
   chosenInlineResult?: ChosenInlineResult;
   newChat?: Chat;
   editedChat?: Chat;
@@ -395,10 +527,16 @@ export function getChatListId(chatList: string) {
 }
 
 export interface GetChatsParams {
-  /** The chat list to get the chats from. Defaults to main. */
+  /**
+   * The chat list to get the chats from. Defaults to main.
+   */
   from?: "main" | "archived";
-  /** The last chat to get chats after. */
+  /**
+   * The last chat to get chats after.
+   */
   after?: Chat;
-  /** The maximum number of results to return. Must be in the range of 1-100. Defaults to 100. */
+  /**
+   * The maximum number of results to return. Must be in the range of 1-100. Defaults to 100.
+   */
   limit?: number;
 }
