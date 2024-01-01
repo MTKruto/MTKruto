560a3f81eab1827855cd44064883d634c20976f0
Roj <ez@roj.im>
2024-01-01 13:21:55 +0300

    Change async-mutex import
    Fixes #152.

--- 0_deps.ts
+++ 0_deps.ts
@@ -18,7 +18,7 @@ export { ctr256, factorize, ige256Decrypt, ige256Encrypt, init as initTgCrypto }
 
 export { gunzip, gzip } from "https://raw.githubusercontent.com/MTKruto/compress/main/gzip/gzip.ts";
 
-export { Mutex, type MutexInterface } from "https://esm.sh/async-mutex@0.4.0";
+export { Mutex } from "https://raw.githubusercontent.com/MTKruto/mutex/main/mod.ts";
 
 export { Parser } from "https://deno.land/x/html_parser@v0.1.3/src/mod.ts";
 
