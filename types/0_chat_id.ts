42cabfb9366031905929a790933b95a7571f6f64
Roj <ez@roj.im>
2024-01-07 19:35:31 +0300

    Rename ChatID to ID

--- types/0_chat_id.ts
+++
@@ -1,4 +0,0 @@
-/**
- * A chat identifier as provided by MTKruto, a chat username, or a chat link (e.g., https://mtkruto.t.me, https://telegram.me/mtkruto).
- */
-export type ChatID = number | string;
---
+++ types/0_id.ts
@@ -0,0 +1,4 @@
+/**
+ * The identifier of a channel, a group, or a user (as provided by MTKruto), a username, or a chat link (e.g., https://mtkruto.t.me, https://telegram.me/mtkruto).
+ */
+export type ID = number | string;
