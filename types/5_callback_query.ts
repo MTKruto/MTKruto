47c5077c79c3bd66fd7839dfefc3b1c49fd16f99
Roj [roːʒ] <rojvv@icloud.com>
2024-01-31 14:30:43 +0300

    Implement methods editInlineMessageText and editInlineMessageReplyMarkup (#169)

--- types/5_callback_query.ts
+++ types/5_callback_query.ts
@@ -1,5 +1,5 @@
-import { base64EncodeUrlSafe, cleanObject, UNREACHABLE } from "../1_utilities.ts";
-import { peerToChatId, serialize, types } from "../2_tl.ts";
+import { base64DecodeUrlSafe, base64EncodeUrlSafe, cleanObject, UNREACHABLE } from "../1_utilities.ts";
+import { peerToChatId, serialize, TLReader, types } from "../2_tl.ts";
 import { EntityGetter } from "./1__getters.ts";
 import { constructUser, User } from "./1_user.ts";
 import { Message, MessageGetter } from "./4_message.ts";
@@ -13,6 +13,22 @@ export interface CallbackQuery {
   chatInstance: string;
   data?: string;
   gameShortName?: string;
+}
+
+const ERR_INVALID_MESSAGE_ID = new Error("Invalid inline message ID");
+export function deserializeInlineMessageId(inlineMessageId: string) {
+  try {
+    const buffer = base64DecodeUrlSafe(inlineMessageId);
+    const reader = new TLReader(buffer);
+    const object = reader.readObject();
+    if (object instanceof types.InputBotInlineMessageID || object instanceof types.InputBotInlineMessageID64) {
+      return object;
+    }
+  } catch {
+    throw ERR_INVALID_MESSAGE_ID;
+  }
+
+  throw ERR_INVALID_MESSAGE_ID;
 }
 
 export async function constructCallbackQuery(callbackQuery: types.UpdateBotCallbackQuery | types.UpdateInlineBotCallbackQuery, getEntity: EntityGetter, getMessage: MessageGetter): Promise<CallbackQuery> {
