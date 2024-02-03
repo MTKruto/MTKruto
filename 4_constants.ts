876ab845ee08a45ebdb8ffdb47a2695c9b313f51
Roj <ez@roj.im>
2024-02-03 17:15:45 +0300

    0.1.148

--- 4_constants.ts
+++ 4_constants.ts
@@ -60,7 +60,7 @@ export const INITIAL_DC: DC = "2";
 
 export const LAYER = 172;
 
-export const APP_VERSION = "MTKruto 0.1.147";
+export const APP_VERSION = "MTKruto 0.1.148";
 
 // @ts-ignore: lib
 export const DEVICE_MODEL = typeof Deno === "undefined" ? typeof navigator === "undefined" ? typeof process === "undefined" ? "Unknown" : process.platform + "-" + process.arch : navigator.userAgent.split(" ")[0] : Deno.build.os + "-" + Deno.build.arch;
