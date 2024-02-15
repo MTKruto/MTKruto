00ebf190b7b9a68635a27b8083a41e8be1bd9762
Roj <ez@roj.im>
2024-02-15 22:38:55 +0300

    Handle messageMediaWebPage when constructing messages

--- types/0_link_preview.ts
+++ types/0_link_preview.ts
@@ -1,7 +1,19 @@
+import { cleanObject } from "../1_utilities.ts";
+import { types } from "../2_tl.ts";
+
 export interface LinkPreview {
   disable?: boolean;
   url?: string;
   smallMedia?: boolean;
   largeMedia?: boolean;
   putAboveText?: boolean;
+}
+
+export function constructLinkPreview(media: types.MessageMediaWebPage, invert?: boolean): LinkPreview {
+  return cleanObject({
+    url: "url" in media.webpage ? media.webpage.url : "",
+    smallMedia: media.force_small_media ? true : undefined,
+    largeMedia: media.force_large_media ? true : undefined,
+    putAboveText: !!invert,
+  });
 }
