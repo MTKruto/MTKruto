a9c13fd8b332f65a37390e360be151d282a821ae
Roj [roːʒ] <rojvv@icloud.com>
2024-02-17 11:34:40 +0300

    Add methods showUsername, hideUsername (#179)

---
+++ client/1_account_manager.ts
@@ -0,0 +1,35 @@
+import { UNREACHABLE } from "../1_utilities.ts";
+import { types } from "../2_tl.ts";
+import { ID } from "../3_types.ts";
+import { C } from "./0_types.ts";
+
+export class AccountManager {
+  #c: C;
+
+  constructor(c: C) {
+    this.#c = c;
+  }
+
+  async #toggleUsername(id: ID, username: string, active: boolean) {
+    const peer = await this.#c.getInputPeer(id);
+    if (peer instanceof types.InputPeerSelf) {
+      await this.#c.api.account.toggleUsername({ username, active });
+    } else if (peer instanceof types.InputPeerUser) {
+      await this.#c.api.bots.toggleUsername({ bot: new types.InputUser(peer), username, active });
+    } else if (peer instanceof types.InputPeerChannel) {
+      await this.#c.api.channels.toggleUsername({ channel: new types.InputChannel(peer), username, active });
+    } else {
+      UNREACHABLE();
+    }
+  }
+
+  async showUsername(id: ID, username: string) {
+    await this.#c.storage.assertUser("showUsername");
+    await this.#toggleUsername(id, username, true);
+  }
+
+  async hideUsername(id: ID, username: string) {
+    await this.#c.storage.assertUser("hideUsername");
+    await this.#toggleUsername(id, username, false);
+  }
+}
