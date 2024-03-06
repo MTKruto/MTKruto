8259d3ede9fdba6a82ef1df9c56f12ef9a61a15a
akumaru <isakulovdev@gmail.com>
2024-03-06 16:30:46 +0500

    added `Poll`

--- types/1_poll.ts
+++ types/1_poll.ts
@@ -5,18 +5,31 @@ import { constructPollOption, PollOption } from "./0_poll_option.ts";
 
 /** A poll. */
 export interface Poll {
+  /** Unique poll identifier. */
   id: string;
+  /** Poll question, 1-300 characters. */
   question: string;
+  /** List of poll options. */
   options: PollOption[];
+  /** Total number of users that voted in the poll. */
   totalVoterCount: number;
+  /** True, if the poll is closed. */
   isClosed: boolean;
+  /** True, if the poll is anonymous. */
   isAnonymous: boolean;
+  /** Poll type, currently can be “regular” or “quiz”. */
   type: "regular" | "quiz";
+  /** True, if the poll allows multiple answers. */
   allowMultipleAnswers?: boolean;
+  /** 0-based identifier of the correct answer option. */
   correctOptionIndex?: number;
+  /** Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters. */
   explanation?: string;
+  /** Special entities like usernames, URLs, bot commands, etc. that appear in the explanation. */
   explanationEntities?: MessageEntity[];
+  /** Amount of time in seconds the poll will be active after creation. */
   openPeriod?: number;
+  /** Point in time (Unix timestamp) when the poll will be automatically closed. */
   closeDate?: Date;
 }
 
