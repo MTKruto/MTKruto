41c6e094afc093cdc3fd001ab3f4553ad64826c7
Roj <ez@roj.im>
2024-03-07 22:02:19 +0300

    a

--- types/0_thumbnail.ts
+++ types/0_thumbnail.ts
@@ -1,5 +1,6 @@
 import { types } from "../2_tl.ts";
-import { FileID, FileType, FileUniqueID, FileUniqueType, ThumbnailSource } from "./0__file_id.ts";
+import { PhotoSourceType } from "../3_types.ts";
+import { FileType, FileUniqueID, FileUniqueType, serializeFileId } from "./0__file_id.ts";
 
 /** A file or sticker thumbnail. */
 export interface Thumbnail {
@@ -18,16 +19,20 @@ export interface Thumbnail {
 export function constructThumbnail(size: types.PhotoSize, file: types.Document | types.Photo): Thumbnail {
   const fileType = file instanceof types.Photo ? FileType.Photo : FileType.Document;
 
-  const fileId = new FileID(null, null, fileType, file.dc_id, {
-    mediaId: file.id,
-    accessHash: file.access_hash,
-    fileReference: file.file_reference,
-    thumbnailFileType: fileType,
-    thumbnailSource: ThumbnailSource.Thumbnail,
-    thumbnailSize: size.type,
-    volumeId: 0n,
-    localId: 0,
-  }).encode();
+  const fileId = file instanceof types.Photo
+    ? serializeFileId({
+      type: FileType.Photo,
+      dcId: file.dc_id,
+      fileReference: file.file_reference,
+      location: { type: "photo", id: file.id, accessHash: file.access_hash, source: { type: PhotoSourceType.Thumbnail, fileType, thumbnailType: size.type.charCodeAt(0) } },
+    })
+    : serializeFileId({
+      type: FileType.Photo,
+      dcId: file.dc_id,
+      fileReference: file.file_reference,
+      location: { type: "common", id: file.id, accessHash: file.access_hash },
+    });
+
   const fileUniqueId = new FileUniqueID(FileUniqueType.Document, { mediaId: file.id }).encode();
 
   return {
