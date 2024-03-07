3d93a7d46a4eeda416cf715376ef18cc2cb78254
akumaru <isakulovdev@gmail.com>
2024-03-07 21:45:02 +0500

    fix `MessageInteractions`

--- types/2_message_interactions.ts
+++ types/2_message_interactions.ts
@@ -2,9 +2,14 @@ import { MessageReaction } from "./1_message_reaction.ts";
 
 /** The interactions made with a message. */
 export interface MessageInteractions {
+  /** ID of the chat where the message was sent. */
   chatId: number;
+  /** Unique identifier of the message. */
   messageId: number;
+  /** The reactions made to the message. */
   reactions: MessageReaction[];
+  /** Number of times the message was viewed. */
   views: number;
+  /** Number of times the message was forwarded. */
   forwards: number;
 }
