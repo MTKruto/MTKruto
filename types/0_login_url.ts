1bf0b86481798889bfb13ccfd20d1002571f9bac
akumarujon <124185383+akumarujon@users.noreply.github.com>
2024-03-08 17:22:06 +0500

    Update types/0_login_url.ts
    Co-authored-by: Roj <rojvv@icloud.com>

--- types/0_login_url.ts
+++ types/0_login_url.ts
@@ -4,7 +4,7 @@ export interface LoginUrl {
   url: string;
   /** A different text for the login button that will be shown when the message is forwarded. */
   forwardText?: string;
-  /** Username of a bot, which will be used for user authorization. */
+  /** The username of the bot that performs the authorization. */
   botUsername?: string;
   /** Pass True to request the permission for your bot to send messages to the user. */
   requestWriteAccess?: boolean;
