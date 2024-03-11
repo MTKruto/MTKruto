be0202208a33138e23bbc508d8abeaa01af2f79c
Roj <ez@roj.im>
2024-03-11 21:38:06 +0300

    Fix thumbnail file IDs

--- types/0_thumbnail.ts
+++ types/0_thumbnail.ts
@@ -16,21 +16,15 @@ export interface Thumbnail {
 }
 
 export function constructThumbnail(size: types.PhotoSize, file: types.Document | types.Photo): Thumbnail {
+  const type = file instanceof types.Photo ? FileType.Photo : FileType.Thumbnail;
   const fileType = file instanceof types.Photo ? FileType.Photo : FileType.Document;
 
-  const fileId_: FileId = file instanceof types.Photo
-    ? {
-      type: FileType.Photo,
-      dcId: file.dc_id,
-      fileReference: file.file_reference,
-      location: { type: "photo", id: file.id, accessHash: file.access_hash, source: { type: PhotoSourceType.Thumbnail, fileType, thumbnailType: size.type.charCodeAt(0) } },
-    }
-    : {
-      type: FileType.Photo,
-      dcId: file.dc_id,
-      fileReference: file.file_reference,
-      location: { type: "common", id: file.id, accessHash: file.access_hash },
-    };
+  const fileId_: FileId = {
+    type,
+    dcId: file.dc_id,
+    fileReference: file.file_reference,
+    location: { type: "photo", id: file.id, accessHash: file.access_hash, source: { type: PhotoSourceType.Thumbnail, fileType, thumbnailType: size.type.charCodeAt(0) } },
+  };
 
   return {
     fileId: serializeFileId(fileId_),
