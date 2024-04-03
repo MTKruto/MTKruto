3319e89cd42cf66cac3ec68a0e7f51223587534a
Roj <ez@roj.im>
2024-04-03 17:36:25 +0300

    Use the same map for messages

--- storage/1_storage_memory.ts
+++ storage/1_storage_memory.ts
@@ -1,10 +1,9 @@
-import { CacheMap, MaybePromise } from "../1_utilities.ts";
+import { MaybePromise } from "../1_utilities.ts";
 import { GetManyFilter, Storage, StorageKeyPart } from "./0_storage.ts";
 import { fromString, isInRange, toString } from "./0_utilities.ts";
 
 export class StorageMemory extends Storage implements Storage {
   protected map: Map<string, unknown> = new Map<string, unknown>();
-  protected messageMap: CacheMap<string, unknown> = new CacheMap<string, unknown>(30_000);
   #id: string | null = null;
   #authString?: string;
 
@@ -21,18 +20,6 @@ export class StorageMemory extends Storage implements Storage {
     if (this.#authString) {
       await this.importAuthString(this.#authString);
     }
-  }
-
-  getMap(key: readonly StorageKeyPart[]): Map<string, unknown> {
-    if (key[0] == "messages.messages") {
-      return this.messageMap;
-    } else {
-      return this.map;
-    }
-  }
-
-  getMaps(): [Map<string, unknown>, Map<string, unknown>] {
-    return [this.map, this.messageMap];
   }
 
   #fixKey(key: readonly StorageKeyPart[]) {
@@ -55,18 +42,16 @@ export class StorageMemory extends Storage implements Storage {
 
   get<T>(key: readonly StorageKeyPart[]): T | null {
     key = this.#fixKey(key);
-    return this.getMap(key).get(toString(key)) as T ?? null;
+    return this.map.get(toString(key)) as T ?? null;
   }
 
   #getEntries() {
     const entries = new Array<[string, unknown]>();
-    for (const map of this.getMaps()) {
-      for (const entry of map.entries()) {
-        if (this.#id !== null && !entry[0].startsWith("__S" + this.#id)) {
-          continue;
-        }
-        entries.push(entry);
+    for (const entry of this.map.entries()) {
+      if (this.#id !== null && !entry[0].startsWith("__S" + this.#id)) {
+        continue;
       }
+      entries.push(entry);
     }
     return entries;
   }
@@ -104,9 +89,9 @@ export class StorageMemory extends Storage implements Storage {
 
     const key = toString(key_);
     if (value != null) {
-      this.getMap(key_).set(key, value);
+      this.map.set(key, value);
     } else {
-      this.getMap(key_).delete(key);
+      this.map.delete(key);
     }
   }
 
