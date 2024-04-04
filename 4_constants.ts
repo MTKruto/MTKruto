0bbc9435695d744b107b0e6b6d266330741a5919
Roj <ez@roj.im>
2024-04-04 18:31:42 +0300

    Remove version from default app version

--- 4_constants.ts
+++ 4_constants.ts
@@ -58,7 +58,7 @@ export const INITIAL_DC: DC = "2";
 
 export const LAYER = 177;
 
-export const APP_VERSION = "MTKruto 0.1.190";
+export const APP_VERSION = "MTKruto";
 
 // @ts-ignore: lib
 export const DEVICE_MODEL: string = typeof Deno === "undefined" ? typeof navigator === "undefined" ? typeof process === "undefined" ? "Unknown" : process.platform + "-" + process.arch : (navigator.userAgent.split(" ")[0] || "Unknown") : Deno.build.os + "-" + Deno.build.arch;
