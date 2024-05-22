b1bfb83bdf5b0cbef6d78a29d50c77cc33f7b8f0
Roj <ez@roj.im>
2024-05-22 21:34:41 +0300

    Fix issues with peerToChatId

--- tl/3_utilities.ts
+++ tl/3_utilities.ts
@@ -31,11 +31,11 @@ export type AnyEntity = Api.user | Api.channel | Api.channelForbidden | Api.chat
 
 export function peerToChatId(peer: Api.Peer | Api.InputPeer | AnyEntity | Api.channelFull | Api.UserFull | Api.chatFull | { channel_id: bigint } | { user_id: bigint } | { chat_id: bigint }): number {
   if (("_" in peer && (peer._ == "peerUser" || peer._ == "inputPeerUser" || peer._ == "user" || peer._ == "userFull")) || "user_id" in peer) {
-    return Number("id" in peer ? peer.id : peer.user_id);
+    return Number("user_id" in peer ? peer.user_id : peer.id);
   } else if (("_" in peer && (peer._ == "peerChat" || peer._ == "inputPeerChat" || peer._ == "chat" || peer._ == "chatForbidden" || peer._ == "chatFull")) || "chat_id" in peer) {
-    return -Number("id" in peer ? peer.id : peer.chat_id);
+    return -Number("chat_id" in peer ? peer.chat_id : peer.id);
   } else if (("_" in peer && (peer._ == "peerChannel" || peer._ == "inputPeerChannel" || peer._ == "channel" || peer._ == "channelForbidden" || peer._ == "channelFull")) || "channel_id" in peer) {
-    return getChannelChatId("id" in peer ? peer.id : peer.channel_id);
+    return getChannelChatId("channel_id" in peer ? peer.channel_id : peer.id);
   } else {
     unreachable();
   }
