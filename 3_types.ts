6cd089491f1ae590e383f3b3d285dc1d283122af
Roj <rojvv@icloud.com>
2024-07-01 13:34:36 +0300

    Add the methods addChatMember and addChatMembers (#228)

--- 3_types.ts
+++ 3_types.ts
@@ -32,6 +32,7 @@ export * from "./types/0_chat_photo.ts";
 export * from "./types/0_connection_state.ts";
 export * from "./types/0_contact.ts";
 export * from "./types/0_dice.ts";
+export * from "./types/0_failed_invitation.ts";
 export * from "./types/0_file_source.ts";
 export * from "./types/0_giveaway_parameters.ts";
 export * from "./types/0_id.ts";
