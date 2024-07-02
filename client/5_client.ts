d063ecb78d60bdc7ae48935ee46b87afcacbb817
Roj <ez@roj.im>
2024-07-02 14:59:13 +0300

    Update sendMediaGroup docs

--- client/5_client.ts
+++ client/5_client.ts
@@ -1766,7 +1766,7 @@ export class Client<C extends Context = Context> extends Composer<C> {
    *
    * @method ms
    * @param chatId The chat to send the media group to.
-   * @param media The media to include in the media group. All of them must be of the same media type.
+   * @param media The media to include in the media group. Animations are not allowed. All of them must be of the same media type, but an exception is that photos and videos can be mixed.
    * @returns The sent messages.
    */
   async sendMediaGroup(chatId: ID, media: InputMedia[], params?: SendMediaGroupParams): Promise<Message[]> {
