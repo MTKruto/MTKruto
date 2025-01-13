df2fd05b6d41648c4399bb795fe906d6f5632329
Roj <ez@roj.im>
2025-01-13 21:12:09 +0300

    Add the method getGifts

--- types/1_sticker.ts
+++ types/1_sticker.ts
@@ -62,10 +62,16 @@ export type StickerSetNameGetter = (inputStickerSet: Api.inputStickerSetID) => M
 
 export async function constructSticker(document: Api.document, fileId: string, fileUniqueId: string, getStickerSetName: StickerSetNameGetter, customEmojiId = ""): Promise<Sticker> {
   const stickerAttribute = document.attributes.find((v): v is Api.documentAttributeSticker => is("documentAttributeSticker", v))!;
+  const setName = is("inputStickerSetID", stickerAttribute.stickerset) ? await getStickerSetName(stickerAttribute.stickerset) : undefined;
+
+  return constructSticker2(document, fileId, fileUniqueId, setName, customEmojiId);
+}
+
+export function constructSticker2(document: Api.document, fileId: string, fileUniqueId: string, setName: string | undefined, customEmojiId = ""): Sticker {
+  const stickerAttribute = document.attributes.find((v): v is Api.documentAttributeSticker => is("documentAttributeSticker", v))!;
   const imageSizeAttribute = document.attributes.find((v): v is Api.documentAttributeImageSize => is("documentAttributeImageSize", v))!;
   const customEmojiAttribute = document.attributes.find((v): v is Api.documentAttributeCustomEmoji => is("documentAttributeCustomEmoji", v));
   const videoAttribute = document.attributes.find((v): v is Api.documentAttributeVideo => is("documentAttributeVideo", v))!;
-  const setName = is("inputStickerSetID", stickerAttribute.stickerset) ? await getStickerSetName(stickerAttribute.stickerset) : undefined;
 
   return cleanObject({
     fileId,
---
+++ types/2_gift_upgraded_component.ts
@@ -0,0 +1,171 @@
+/**
+ * MTKruto - Cross-runtime JavaScript library for building Telegram clients
+ * Copyright (C) 2023-2025 Roj <https://roj.im/>
+ *
+ * This file is part of MTKruto.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { unreachable } from "../0_deps.ts";
+import { cleanObject, fromUnixTimestamp } from "../1_utilities.ts";
+import { Api, is } from "../2_tl.ts";
+import { FileId, FileType, serializeFileId, toUniqueFileId } from "./_file_id.ts";
+import { constructMessageEntity, MessageEntity } from "./0_message_entity.ts";
+import { constructSticker2, Sticker } from "./1_sticker.ts";
+
+/**
+ * The model of an upgraded gift.
+ *
+ * @unlisted
+ */
+export interface GiftUpgradedComponentModel {
+  /** @discriminator */
+  type: "model";
+  /** The name of the model. */
+  name: string;
+  /** The sticker belonging to the model. */
+  sticker: Sticker;
+  /** A floating point number determining how rare this type of model is. */
+  rarityPercentage: number;
+}
+
+/**
+ * The pattern of an upgraded gift.
+ *
+ * @unlisted
+ */
+export interface GiftUpgradedComponentPattern {
+  /** @discriminator */
+  type: "pattern";
+  /** The name of the pattern. */
+  name: string;
+  /** The sticker belonging to the pattern. */
+  sticker: Sticker;
+  /** A floating point number determining how rare this type of pattern is. */
+  rarityPercentage: number;
+}
+
+/**
+ * The backdrop of an upgraded gift.
+ *
+ * @unlisted
+ */
+export interface GiftUpgradedComponentBackdrop {
+  /** @discriminator */
+  type: "backdrop";
+  /** The name of the backdrop. */
+  name: string;
+  /** The center color of the backdrop. */
+  centerColor: number;
+  /** The edge color of the backdrop. */
+  edgeColor: number;
+  /** The pattern color of the backdrop. */
+  patternColor: number;
+  /** The text color of the backdrop. */
+  textColor: number;
+  /** A floating point number determining how rare this type of pattern is. */
+  rarityPercentage: number;
+}
+
+/**
+ * The original details of an upgraded gift.
+ *
+ * @unlisted
+ */
+export interface GiftUpgradedComponentOriginalDetails {
+  /** @discriminator */
+  type: "originalDetails";
+  senderId?: number;
+  recipientId: number;
+  date: Date;
+  message?: string;
+  entities?: MessageEntity[];
+}
+
+/**
+ * A component of an upgraded gift.
+ *
+ * @unlisted
+ */
+export type GiftUpgradedComponent = GiftUpgradedComponentModel | GiftUpgradedComponentPattern | GiftUpgradedComponentBackdrop | GiftUpgradedComponentOriginalDetails;
+
+export function constructGiftUpgradedComponent(attribute: Api.StarGiftAttribute): GiftUpgradedComponent {
+  const name = "name" in attribute ? attribute.name : "";
+  const rarityPercentage = "rarity_permille" in attribute ? rarityPerMilleToPercentage(attribute.rarity_permille) : 0;
+
+  switch (attribute._) {
+    case "starGiftAttributeModel": {
+      if (!is("document", attribute.document)) {
+        unreachable();
+      }
+      const fileId: FileId = {
+        type: FileType.Sticker,
+        dcId: attribute.document.dc_id,
+        fileReference: attribute.document.file_reference,
+        location: { type: "common", id: attribute.document.id, accessHash: attribute.document.access_hash },
+      };
+      const sticker = constructSticker2(attribute.document, serializeFileId(fileId), toUniqueFileId(fileId), undefined, "");
+      return {
+        type: "model",
+        name,
+        sticker,
+        rarityPercentage,
+      };
+    }
+    case "starGiftAttributePattern": {
+      if (!is("document", attribute.document)) {
+        unreachable();
+      }
+      const fileId: FileId = {
+        type: FileType.Sticker,
+        dcId: attribute.document.dc_id,
+        fileReference: attribute.document.file_reference,
+        location: { type: "common", id: attribute.document.id, accessHash: attribute.document.access_hash },
+      };
+      const sticker = constructSticker2(attribute.document, serializeFileId(fileId), toUniqueFileId(fileId), undefined, "");
+      return {
+        type: "pattern",
+        name,
+        sticker,
+        rarityPercentage,
+      };
+    }
+    case "starGiftAttributeBackdrop":
+      return {
+        type: "backdrop",
+        name,
+        centerColor: attribute.center_color,
+        edgeColor: attribute.edge_color,
+        patternColor: attribute.pattern_color,
+        textColor: attribute.text_color,
+        rarityPercentage,
+      };
+    case "starGiftAttributeOriginalDetails":
+      return cleanObject({
+        type: "originalDetails",
+        senderId: attribute.sender_id ? Number(attribute.sender_id) : undefined,
+        recipientId: Number(attribute.recipient_id),
+        date: fromUnixTimestamp(attribute.date),
+        message: attribute.message?.text,
+        entities: attribute.message ? attribute.message.entities.map(constructMessageEntity).filter((v): v is MessageEntity => !!v) : undefined,
+      });
+    default:
+      unreachable();
+  }
+}
+
+export function rarityPerMilleToPercentage(perMille: number) {
+  return perMille / 1_000;
+}
