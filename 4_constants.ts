c30d1148d58dacf3749656c7cb673f46a904a4ad
Roj <ez@roj.im>
2025-03-08 15:01:20 +0300

    Update to layer 200

--- 4_constants.ts
+++ 4_constants.ts
@@ -76,7 +76,7 @@ export const PUBLIC_KEYS: PublicKeys = Object.freeze([
 
 export const INITIAL_DC: DC = "2";
 
-export const LAYER = 199;
+export const LAYER = 200;
 
 export const APP_VERSION = "MTKruto";
 
