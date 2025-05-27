1367cd262f686c59a00341e76ae66d3cf08419d2
Roj <ez@roj.im>
2025-05-27 08:55:36 +0300

    Fix CI

--- utilities/1_base64_test.ts
+++ utilities/1_base64_test.ts
@@ -19,6 +19,7 @@
  */
 
 import { assertEquals } from "../0_deps.ts";
+import { encodeText } from "./0_text.ts";
 import { base64DecodeUrlSafe, base64EncodeUrlSafe } from "./1_base64.ts";
 
 Deno.test("base64DecodeUrlSafe", () => {
