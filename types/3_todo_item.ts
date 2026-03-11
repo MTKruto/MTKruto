c6c2539ec479cf451540dfa2c62b275db1a6af19
Roj <rojvv@icloud.com>
2026-03-11 23:16:47 +0300

    Implement to-do lists

---
+++ types/3_todo_item.ts
@@ -0,0 +1,31 @@
+/**
+ * MTKruto - Cross-runtime JavaScript library for building Telegram clients
+ * Copyright (C) 2023-2026 Roj <https://roj.im/>
+ *
+ * This file is part of MTKruto.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import type { MessageEntity } from "./2_message_entity.ts";
+
+/** A to-do item. */
+export interface TodoItem {
+  /** The identifier of the item. */
+  id: number;
+  /** The item's text. */
+  text: string;
+  /** The entities of the text. */
+  entities: MessageEntity[];
+}
