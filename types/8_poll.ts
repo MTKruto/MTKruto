271c5a6ca4c2d6d2974c7fb23c4f0269a48ae48b
Roj <rojvv@icloud.com>
2026-06-17 14:52:20 +0300

    Add the field media and explanationMedia to Poll

--- types/8_poll.ts
+++ types/8_poll.ts
@@ -24,6 +24,7 @@ import { Api } from "../2_tl.ts";
 import type { PeerGetter } from "./1_chat_p.ts";
 import type { StickerSetNameGetter } from "./1_sticker.ts";
 import { constructMessageEntity, type MessageEntity } from "./2_message_entity.ts";
+import { constructPollMedia, type PollMedia } from "./6_poll_media.ts";
 import { constructPollOption, type PollOption } from "./7_poll_option.ts";
 
 /** A poll. */
@@ -34,6 +35,8 @@ export interface Poll {
   question: string;
   /** The entities of the poll's question. */
   questionEntities: MessageEntity[];
+  /** The poll's media. */
+  media?: PollMedia;
   /** The poll's options. */
   options: PollOption[];
   /** The number of users who have participated in the poll. */
@@ -52,6 +55,8 @@ export interface Poll {
   explanation?: string;
   /** The explanation's entities. */
   explanationEntities?: MessageEntity[];
+  /** The explanation's media. */
+  explanationMedia?: PollMedia;
   /** Duration of the poll in seconds. */
   openPeriod?: number;
   /** The time when the poll will be closed. */
@@ -62,12 +67,14 @@ export interface Poll {
 
 export async function constructPoll(media_: Api.messageMediaPoll, getStickerSetName: StickerSetNameGetter, getPeer: PeerGetter): Promise<Poll> {
   const poll = media_.poll;
+
   const correctOptions = media_.results.results?.filter((v) => v.correct).map((v) => v.option);
   const correctOptionIndexes = correctOptions !== undefined ? poll.answers.filter((v) => correctOptions.some((v_) => equals(v_, Api.as("pollAnswer", v).option))).map((_, i) => i) : undefined;
   return cleanObject({
     id: String(poll.id),
     question: poll.question.text,
     questionEntities: poll.question.entities.map(constructMessageEntity).filter((v): v is MessageEntity => v !== null),
+    media: media_.attached_media ? await constructPollMedia(media_.attached_media, getStickerSetName, getPeer) : undefined,
     options: await Promise.all(poll.answers.map((v) => constructPollOption(v, media_.results.results ?? [], getStickerSetName, getPeer))),
     totalVoterCount: media_.results.total_voters ?? 0,
     isClosed: poll.closed || false,
@@ -77,6 +84,7 @@ export async function constructPoll(media_: Api.messageMediaPoll, getStickerSetN
     correctOptionIndexes,
     explanation: media_.results.solution,
     explanationEntities: media_.results.solution_entities?.map(constructMessageEntity).filter((v): v is MessageEntity => v !== null),
+    explanationMedia: media_.results.solution_media ? await constructPollMedia(media_.results.solution_media, getStickerSetName, getPeer) : undefined,
     openPeriod: poll.close_period,
     closeDate: poll.close_date,
     countries: poll.countries_iso2,
