a99a622544506259d7bb8d9ebc7fa346b4bbaa92
Roj <rojvv@icloud.com>
2026-06-24 16:41:10 +0300

    Nitpicks

--- client/3_secret_chat_manager.ts
+++ client/3_secret_chat_manager.ts
@@ -664,14 +664,14 @@ export class SecretChatManager implements UpdateProcessor<SecretChatManagerUpdat
   }
 
   async sendSecretTypingAction(id: number) {
-    this.#c.storage.assertUser("sendSecretChatTypingAction");
+    this.#c.storage.assertUser("sendSecretTypingAction");
     const { encryptedChat } = this.#mustGetEncryptedChat(id);
     const peer: Api.inputEncryptedChat = { _: "inputEncryptedChat", chat_id: encryptedChat.id, access_hash: encryptedChat.access_hash };
     await this.#c.invoke({ _: "messages.setEncryptedTyping", peer, typing: true });
   }
 
   async sendSecretCancelTypingAction(id: number) {
-    this.#c.storage.assertUser("sendSecretChatCancelTypingAction");
+    this.#c.storage.assertUser("sendSecretCancelTypingAction");
     const { encryptedChat } = this.#mustGetEncryptedChat(id);
     const peer: Api.inputEncryptedChat = { _: "inputEncryptedChat", chat_id: encryptedChat.id, access_hash: encryptedChat.access_hash };
     await this.#c.invoke({ _: "messages.setEncryptedTyping", peer, typing: false });
