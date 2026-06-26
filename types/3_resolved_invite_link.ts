24cbcba078e26001e2c3aa058b603964eb66b437
Roj <rojvv@icloud.com>
2026-06-26 16:09:45 +0300

    Document ResolvedInviteLink

--- types/3_resolved_invite_link.ts
+++ types/3_resolved_invite_link.ts
@@ -23,24 +23,43 @@ import { Api } from "../2_tl.ts";
 import { constructPhoto, type Photo } from "./1_photo.ts";
 import { constructUser, type User } from "./2_user.ts";
 
+/** A resolved invite link. */
 export interface ResolvedInviteLink {
+  /** The type of the chat. */
   type: "group" | "supergroup" | "channel";
+  /** Whether the chat is public. */
   isPublic: boolean;
+  /** Whether approval is needed by an admin of the chat prior to joining. */
   isApprovalNeeded: boolean;
+  /** Whether the chat is verified. */
   isVerified: boolean;
+  /** Whether the chat has been identified as a scam. */
   isScam: boolean;
+  /** Whether the chat has been identifier as a fake. */
   isFake: boolean;
+  /** Whether no repayment is required to join the chat. */
   isAlreadySubscribed: boolean;
+  /** The title of the chat. */
   title: string;
+  /** The description of the chat. */
   description?: string;
+  /** The chat's photo. */
   photo?: Photo;
+  /** The chat's member count. */
   memberCount: number;
+  /** A preview of the chat's members. */
   members: User[];
+  /** The chat's color. */
   color: number;
+  /** The chat's subscription period. */
   subscriptionPeriod?: number;
+  /** The chat's subscription price. */
   subscriptionPrice?: number;
+  /** The chat verification bot's identifier. */
   verificationBotId?: number;
+  /** The chat verification's custom emoji identifier. */
   verificationCustomEmojiId?: number;
+  /** The chat verification's description. */
   verificationDescription?: string;
 }
 
