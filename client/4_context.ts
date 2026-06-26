82402a1ffc89d7f347a12e3f225e090560d9575a
Roj <rojvv@icloud.com>
2026-06-26 19:50:29 +0300

    ci

--- client/4_context.ts
+++ client/4_context.ts
@@ -1583,7 +1583,7 @@ export class Context {
   /** Context-aware alias for {@link Client.addContact}. */
   async saveDraft(text: string, params?: SaveDraftParams): Promise<void> {
     const chatId = this.#mustGetChatId();
-    return await this.client.saveRichTextDraft(chatId, text, params);
+    return await this.client.saveDraft(chatId, text, params);
   }
 
   /** Context-aware alias for {@link Client.saveRichTextDraft}. */
